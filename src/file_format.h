b9e0abcaa8fef8dd4187b099b1f5bce290238061
Pete Curtis <pcurtis3@comcast.net>
2012-02-19 23:14:32 -0600

    Added Doxyfile and some documentation

--- src/file_format.h
+++ src/file_format.h
@@ -29,7 +29,6 @@
 class Importer;
 class Exporter;
 
-
 /** An exception type thrown by an importer or exporter if it encounters a fatal error.
  */
 class FormatException : public std::exception
@@ -46,24 +45,79 @@ private:
 };
 
 
-/** Describes a file format understood by this application.
+/** Describes a file format understood by this application. Each file format has an ID (an internal, non-translated
+ *  string); a description (translated); a file extension (non-translated); and methods to indicate their support
+ *  for import or export. Formats are installed into a FormatRegistry, and can be looked up in a variety of ways.
+ *
+ *  A typical Format subclass will look like this:
+ *
+ *  \code
+ *  class MyCustomFileFormat {
+ *  public:
+ *      MyCustomFileFormat : Format("custom", QObject::tr("Custom file"), "custom", true, true) {
+ *      }
+ *
+ *      Importer *createImporter(const QString &path, Map *map, MapView *view) const throw (FormatException) {
+ *          return new CustomImporter(path, map, view);
+ *      }
+ *
+ *      Exporter *createExporter(const QString &path, Map *map, MapView *view) const throw (FormatException) {
+ *          return new CustomExporter(path, map, view);
+ *      }
+ *  }
+ *  \endcode
  */
 class Format
 {
 public:
+    /** Creates a new file format with the given parameters. Don't use a leading dot on the file extension.
+     */
     Format(const QString &id, const QString &description, const QString &file_extension, bool supportsImport = true, bool supportsExport = true);
+
     virtual ~Format() {}
 
+    /** Returns the internal ID of the file format.
+     */
     const QString &id() const { return format_id; }
+
+    /** Returns a human-readable description of the file format.
+     */
     const QString &description() const { return format_description; }
+
+    /** Returns the file extension used by this file format.
+     */
     const QString &fileExtension() const { return file_extension; }
 
+    /** Returns true if this file format supports importing a map from its associated file type.
+     */
     bool supportsImport() const { return supports_import; }
+
+    /** Returns true if this file format supports exporting a map to its associated file type.
+     */
     bool supportsExport() const { return supports_export; }
 
+    /** Returns true if this file format believes it is capable of understanding a file that
+     *  starts with the given byte sequence. "Magic" numbers and version information is commonly
+     *  placed at the beginning of a file, and this method is used by the application to pre-screen
+     *  for a suitable Importer. If there is any doubt about whether the file format can successfully
+     *  process a file, this method should return false.
+     */
     virtual bool understands(const unsigned char *buffer, size_t sz) const;
 
+    /** Creates an Importer that will read the file at the given path into the given map and view.
+     *  The caller can then call doImport() in the returned object to start the import process. The caller
+     *  is responsible for deleting the Importer when it's finished.
+     *
+     *  If the Importer could not be created, then this method should throw a FormatException.
+     */
     virtual Importer *createImporter(const QString &path, Map *map, MapView *view) const throw (FormatException);
+
+    /** Creates an Exporter that will save the given map and view into a file at the given path.
+     *  The caller can then call doExport() in the returned object to start the export process. The caller
+     *  is responsible for deleting the Exporter when it's finished.
+     *
+     *  If the Exporter could not be created, then this method should throw a FormatException.
+     */
     virtual Exporter *createExporter(const QString &path, Map *map, MapView *view) const throw (FormatException);
 
 private:
@@ -75,19 +129,40 @@ private:
 
 };
 
-/** Provides a registry for formats, and takes ownership of the supported format objects.
+/** Provides a registry for file formats, and takes ownership of the supported format objects.
  */
 class FormatRegistry
 {
 public:
+    /** Creates an empty file format registry.
+     */
     FormatRegistry() {};
+
+    /** Destroys a file format registry.
+     */
     ~FormatRegistry();
 
+    /** Returns an immutable list of the available file formats.
+     */
     inline const std::vector<Format *> &formats() const { return fmts; }
+
+    /** Finds a file format with the given internal ID, or returns NULL if no format
+     *  is found.
+     */
     const Format *findFormat(const QString &id) const;
+
+    /** Finds a file format whose file extension matches the fie extension of the given
+     *  path, or returns NULL if no matching format is found.
+     */
     const Format *findFormatForFilename(const QString &filename) const;
+
+    /** Returns the ID of default file format for this registry. This will automatically
+     *  be set to the first registered format.
+     */
     const QString &defaultFormat() const { return default_format_id; }
 
+    /** Registers a new file format. The registry takes ownership of the provided Format.
+     */
     void registerFormat(Format *format);
 
 private:
@@ -95,25 +170,50 @@ private:
     QString default_format_id;
 };
 
+/** A FormatRegistry that is globally defined for convenience. Within the scope of a single
+ *  application, you can simply use calls of the form "FileFormats.findFormat(...)".
+ */
 extern FormatRegistry FileFormats;
 
 
-/** Base class for both importer and exporters; provides support for configuring the map and
+
+/** Abstract base class for both importer and exporters; provides support for configuring the map and
  *  view to manipulate, setting and retrieving options, and collecting a list of warnings.
- *  Also stores the registry of importers and exporters
+ *
+ *  Subclasses should define default values for options they intend to use in their constructors,
+ *  by calling setOption() with the relevant values. There is no such thing as an "implicit default"
+ *  for options.
  */
 class ImportExport
 {
 public:
+    /** Creates a new importer or exporter with the given file path, map, and view.
+     */
     ImportExport(const QString &path, Map *map, MapView *view) : path(path), map(map), view(view) {}
+
+    /** Destroys an importer or exporter.
+     */
     virtual ~ImportExport() {}
 
+    /** Returns the current list of warnings collected by this object.
+     */
     inline const std::vector<QString> &warnings() const { return warn; }
+
+    /** Sets an option in this importer or exporter.
+     */
     inline void setOption(const QString &name, QVariant value) { options[name] = value; }
+
+    /** Retrieves the value of an options in this importer or exporter. If the option does not have
+     *  a value - either a default value assigned in the constructor, or a custom value assigned
+     *  through setOption() - then a FormatException will be thrown.
+     */
     QVariant option(const QString &name) const throw (FormatException);
 
 
 protected:
+    /** Adds an import/export warning to the current list of warnings. The provided message
+     *  should be translated.
+     */
     inline void addWarning(const QString &str) { warn.push_back(str); }
 
 protected:
@@ -144,31 +244,52 @@ class ImportAction
 
 
 /** Base class for all importers. An Importer has the following lifecycle:
- *
- *  1. The Importer is constructed, with pointers to the map and view. The Importer
+ *  -# The Importer is constructed, with pointers to the map and view. The Importer
  *     should also set default values for any options it will read. The base class
  *     will throw an exception if the importer reads an option that does not have a value.
- *  2. setOption() will be called zero or more times to customize the options.
- *  3. doImport() will be called to perform the initial import. The implementation of
+ *  -# setOption() will be called zero or more times to customize the options.
+ *  -# doImport() will be called to perform the initial import. The implementation of
  *     this method will try to populate the map and view from the given file, and may
- *     optionally register action items for the user via addActionItem().
- *  4. If action items are present, then they will be presented to the user. Each
+ *     optionally register action items for the user via addAction().
+ *  -# If action items are present, then they will be presented to the user. Each
  *     action item will have its satisfy() method called with the user's choice.
- *  5. finishImport() will be called. If any action items were created, this method
+ *  -# finishImport() will be called. If any action items were created, this method
  *     should finish the import based on the values supplied by the user.
  */
 class Importer : public ImportExport
 {
 public:
+    /** Creates a new Importer with the given file path, map, and view.
+     */
     Importer(const QString &path, Map *map, MapView *view) : ImportExport(path, map, view) {}
+
+    /** Destroys this Importer.
+     */
     virtual ~Importer() {}
 
+    /** Returns the current list of action items.
+     */
     inline const std::vector<ImportAction> &actions() const { return act; }
 
+    /** Begins the import process. The implementation of this method should read the file
+     *  and populate the map and view from it. If a fatal error is encountered (such as a
+     *  missing or corrupt file), than it should throw a FormatException. If the import can
+     *  proceed, but information might be lost in the process, than it should call
+     *  addWarning() with a translated, useful description of the issue. The line between
+     *  errors and warnings is somewhat of a judgement call on the part of the author, but
+     *  generally an Importer should not succeed unless the map is populated sufficiently
+     *  to be useful.
+     */
     virtual void doImport() throw (FormatException) = 0;
+
+    /** Once all action items are satisfied, this method should be called to complete the
+     *  import process. This class defines a default implementation, that does nothing.
+     */
     virtual void finishImport() throw (FormatException) {}
 
 protected:
+    /** Adds an action item to the current list.
+     */
     inline void addAction(const ImportAction &action) { act.push_back(action); }
 
 private:
@@ -188,13 +309,20 @@ private:
 class Exporter : public ImportExport
 {
 public:
+    /** Creates a new Importer with the given file path, map, and view.
+     */
     Exporter(const QString &path, Map *map, MapView *view) : ImportExport(path, map, view) {}
+
+    /** Destroys the current Exporter.
+     */
     virtual ~Exporter() {}
 
+    /** Exports the map and view to the given file. If a fatal error is encountered (such as a
+     *  permission problem), than this method should throw a FormatException. If the export can
+     *  proceed, but information might be lost in the process, than it should call
+     *  addWarning() with a translated, useful description of the issue.
+     */
     virtual void doExport() throw (FormatException) = 0;
 };
-
-
-
 
 #endif // IMPORT_EXPORT_H
--- src/file_format_native.h
+++ src/file_format_native.h
@@ -22,39 +22,79 @@
 
 #include "file_format.h"
 
+/** Provides a description of the native file format. Currently this is a (arch-dependent) binary packed format
+ *  with a file extension of "omap", and internal versioning.
+ */
 class NativeFileFormat : public Format
 {
 public:
+    /** Creates a new file format representing the native type.
+     */
     NativeFileFormat() : Format("native", QObject::tr("Open Orienteering Mapper"), "omap", true, true) {}
 
+    /** Returns true if the file starts with the magic byte sequence "OMAP" (0x4f 0x4d 0x41 0x50).
+     */
     bool understands(const unsigned char *buffer, size_t sz) const;
+
+    /** Creates an importer for this file type.
+     */
     Importer *createImporter(const QString &path, Map *map, MapView *view) const throw (FormatException);
+
+    /** Creates an exporter for this file type.
+     */
     Exporter *createExporter(const QString &path, Map *map, MapView *view) const throw (FormatException);
 
+    /** Constant describing the earliest OMAP version supported by this file format.
+     */
     static const int least_supported_file_format_version;
+
+    /** Constant describing the current OMAP version used by this file format for saving.
+     */
     static const int current_file_format_version;
+
+    /** The file magic: "OMAP"
+     */
     static const char magic_bytes[4];
 };
 
 
+/** An Importer for the native file format. This class delegates to the load() and loadImpl() methods of the
+ *  model objects, but long-term this can be refactored out of the model into this class.
+ */
 class NativeFileImport : public Importer
 {
 public:
+    /** Creates a new native file importer.
+     */
     NativeFileImport(const QString &path, Map *map, MapView *view);
+
+    /** Destroys this importer.
+     */
     ~NativeFileImport();
 
+    /** Imports a native file.
+     */
     void doImport() throw (FormatException);
 };
 
 
+/** An Exporter for the native file format. This class delegates to the save() and saveImpl() methods of the
+ *  model objects, but long-term this can be refactored out of the model into this class.
+ */
 class NativeFileExport : public Exporter
 {
 public:
+    /** Creates a new native file exporter.
+     */
     NativeFileExport(const QString &path, Map *map, MapView *view);
+
+    /** Destroys this importer.
+     */
     ~NativeFileExport();
 
+    /** Exports a native file.
+     */
     void doExport() throw (FormatException);
 };
-
 
 #endif // NATIVE_FILE_FORMAT_H
