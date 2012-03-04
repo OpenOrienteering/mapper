77740c47907efc9c2ca0db8495ef774c4d272559
Kai Pastor <dg0yt@darc.de>
2012-03-04 23:25:48 +0100

    Added .deb packaging based CPack

---
+++ CPackaging.cmake
@@ -0,0 +1,63 @@
+# Packaging with CPack
+if(EXISTS "${CMAKE_ROOT}/Modules/CPack.cmake")
+
+	include(InstallRequiredSystemLibraries)
+
+	# sources not properly configured
+	set(CPACK_SOURCE_GENERATOR "OFF")
+
+	# cf. http://www.cmake.org/cmake/help/cmake-2-8-docs.html#module:CPack
+	# cf. http://www.cmake.org/Wiki/CMake:CPackPackageGenerators
+	set(CPACK_PACKAGE_Name "OpenOrienteering Mapper")
+	set(CPACK_PACKAGE_VENDOR "OpenOrienteering Developers")
+	set(CPACK_PACKAGE_VERSION_MAJOR "0")
+	set(CPACK_PACKAGE_VERSION_MINOR "1")
+	set(CPACK_PACKAGE_VERSION_PATCH "1")
+	set(CPACK_PACKAGE_DESCRIPTION_SUMMARY 
+	  "Map drawing program from OpenOrienteering")
+	set(CPACK_PACKAGE_FILE_NAME 
+	  "oorienteering-mapper_${CPACK_PACKAGE_VERSION_MAJOR}.${CPACK_PACKAGE_VERSION_MINOR}.${CPACK_PACKAGE_VERSION_PATCH}-${CMAKE_SYSTEM_PROCESSOR}")
+	set(CPACK_RESOURCE_FILE_LICENSE "../COPYING")
+	set(CPACK_STRIP_FILES "TRUE")
+
+	if(UNIX AND EXISTS /usr/bin/dpkg AND EXISTS /usr/bin/lsb_release)
+		# Packaging on Debian or similar
+		set(CPACK_GENERATOR "DEB")
+		message(STATUS
+		  "ATTENTION: 
+   To build a .deb package with proper file ownership, you must run 
+   'fakeroot make package'.")
+		execute_process(
+		  COMMAND /usr/bin/lsb_release -sc 
+		  OUTPUT_VARIABLE CPACK_LSB_RELEASE 
+		  OUTPUT_STRIP_TRAILING_WHITESPACE)
+		string(REPLACE
+		  ${CMAKE_SYSTEM_PROCESSOR} 
+		  "${CPACK_LSB_RELEASE}_${CMAKE_SYSTEM_PROCESSOR}" 
+		  CPACK_PACKAGE_FILE_NAME
+		  ${CPACK_PACKAGE_FILE_NAME})
+		string(REPLACE 
+		  "x86_64" 
+		  "amd64" 
+		  CPACK_PACKAGE_FILE_NAME
+		  ${CPACK_PACKAGE_FILE_NAME})
+		set(CPACK_DEBIAN_PACKAGE_NAME "oorienteering-mapper")
+		set(CPACK_DEBIAN_PACKAGE_MAINTAINER
+		   "OpenOrienteering Developers <dg0yt@darc.de>")
+		set(CPACK_DEBIAN_SECTION "graphics")
+		set(CPACK_DEBIAN_PACKAGE_HOMEPAGE 
+		  "http://oorienteering.sourceforge.net/")
+		set(CPACK_DEBIAN_PACKAGE_SHLIBDEPS "ON")
+
+		install(
+		  TARGETS Mapper 
+		  DESTINATION bin)
+		install(
+		  FILES COPYING 
+		  DESTINATION "share/doc/${CPACK_DEBIAN_PACKAGE_NAME}")
+
+	endif(UNIX AND EXISTS /usr/bin/dpkg AND EXISTS /usr/bin/lsb_release)
+
+	include(CPack)
+
+endif(EXISTS "${CMAKE_ROOT}/Modules/CPack.cmake")
