834344240cae09143cd2ba95fbc1af016927dde4
Kai Pastor <dg0yt@darc.de>
2012-03-25 10:33:11 +0200

    Minor cleanup, mostly whitespace.

--- src/main.cpp
+++ src/main.cpp
@@ -32,56 +32,54 @@
 
 int main(int argc, char** argv)
 {
-    // Create single-instance application.
-    // Use "oo-mapper" instead of the executable as identifier, in case we launch from different paths.
-    QtSingleApplication qapp("oo-mapper", argc, argv);
-    if (qapp.isRunning()) {
-        // Send a message to activate the running app, and optionally open a file
-        qapp.sendMessage((argc > 1) ? argv[1] : "");
-        return 0;
-    }
-
-	MainWindow* first_window = NULL;
+	// Create single-instance application.
+	// Use "oo-mapper" instead of the executable as identifier, in case we launch from different paths.
+	QtSingleApplication qapp("oo-mapper", argc, argv);
+	if (qapp.isRunning()) {
+		// Send a message to activate the running app, and optionally open a file
+		qapp.sendMessage((argc > 1) ? argv[1] : "");
+		return 0;
+	}
 	
 	// Set settings defaults
 	QCoreApplication::setOrganizationName("Thomas Schoeps");
 	QCoreApplication::setApplicationName("OpenOrienteering");
 	
+#ifdef WIN32
 	// Load plugins on Windows
-	#ifdef WIN32
-		qapp.addLibraryPath(QCoreApplication::applicationDirPath() + "/plugins");
-	#endif
+	qapp.addLibraryPath(QCoreApplication::applicationDirPath() + "/plugins");
+#endif
 	
 	// Localization
 	QString locale = QLocale::system().name();
 	QTranslator qtTranslator;
-	#ifdef WIN32
-		qtTranslator.load("qt_" + locale, "translations/");
-	#else
-		qtTranslator.load("qt_" + locale, QLibraryInfo::location(QLibraryInfo::TranslationsPath));
-	#endif
+#ifdef WIN32
+	qtTranslator.load("qt_" + locale, "translations/");
+#else
+	qtTranslator.load("qt_" + locale, QLibraryInfo::location(QLibraryInfo::TranslationsPath));
+#endif
 	qapp.installTranslator(&qtTranslator);
 	QTranslator translator;
 	translator.load(locale, QString(":/translations"));
 	qapp.installTranslator(&translator);
 	
-    // Register the supported file formats
-    FileFormats.registerFormat(new NativeFileFormat());
-    FileFormats.registerFormat(new XMLFileFormat());
-    FileFormats.registerFormat(new OCAD8FileFormat());
-
+	// Register the supported file formats
+	FileFormats.registerFormat(new NativeFileFormat());
+	FileFormats.registerFormat(new XMLFileFormat());
+	FileFormats.registerFormat(new OCAD8FileFormat());
+	
 	// Create first main window
-	first_window = new MainWindow(true);
+	MainWindow* first_window = new MainWindow(true);
 	first_window->setController(new HomeScreenController());
 	
 	// Treat all program parameters as files to be opened
 	for (int i = 1; i < argc; i++)
 		first_window->openPath(argv[i]);
-
-    // If we need to respond to a second app launch, do so, but also accept a file open request.
-    qapp.setActivationWindow(first_window);
-    QObject::connect(&qapp, SIGNAL(messageReceived(const QString&)), first_window, SLOT(openPath(const QString &)));
-
+	
+	// If we need to respond to a second app launch, do so, but also accept a file open request.
+	qapp.setActivationWindow(first_window);
+	QObject::connect(&qapp, SIGNAL(messageReceived(const QString&)), first_window, SLOT(openPath(const QString &)));
+	
 	// Let application run
 	first_window->show();
 	first_window->raise();
