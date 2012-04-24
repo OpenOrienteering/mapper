OpenOrienteering/mapper

commit 699e0b48b1c9f208d2356af2019fcdaf84a537eb

Implement the circle drawing tool

src/tool_draw_path.cpp
@@ -23,34 +23,17 @@
 #include <QtGui>
 
 #include "util.h"
-#include "symbol_dock_widget.h"
 #include "symbol.h"
 #include "object.h"
 #include "map_widget.h"
-#include "symbol_point.h"
-#include "symbol_line.h"
-#include "symbol_combined.h"
-#include "map_undo.h"
 
 QCursor* DrawPathTool::cursor = NULL;
 
 DrawPathTool::DrawPathTool(MapEditorController* editor, QAction* tool_button, SymbolWidget* symbol_widget, bool allow_closing_paths)
- : MapEditorTool(editor, Other, tool_button), renderables(editor->getMap()), symbol_widget(symbol_widget), allow_closing_paths(allow_closing_paths)
+ : DrawLineAndAreaTool(editor, tool_button, symbol_widget), allow_closing_paths(allow_closing_paths)
 {
 	dragging = false;
-	draw_in_progress = false;
-	preview_points_shown = false;
-	path_combination = NULL;
-	preview_path = NULL;
-	last_used_symbol = NULL;
-	is_helper_tool = symbol_widget == NULL;
 	space_pressed = false;
-	
-	selectedSymbolsChanged();
-	if (symbol_widget)
-		connect(symbol_widget, SIGNAL(selectedSymbolsChanged()), this, SLOT(selectedSymbolsChanged()));
-	connect(editor->getMap(), SIGNAL(symbolChanged(int,Symbol*,Symbol*)), this, SLOT(symbolChanged(int,Symbol*,Symbol*)));
-	connect(editor->getMap(), SIGNAL(symbolDeleted(int,Symbol*)), this, SLOT(symbolDeleted(int,Symbol*)));
 	
 	if (!cursor)
 		cursor = new QCursor(QPixmap(":/images/cursor-draw-path.png"), 11, 11);
@@ -58,11 +41,6 @@ DrawPathTool::DrawPathTool(MapEditorController* editor, QAction* tool_button, Sy
 void DrawPathTool::init()
 {
 	updateStatusText();
-}
-DrawPathTool::~DrawPathTool()
-{
-	deleteObjects();
-	delete path_combination;
 }
 
 bool DrawPathTool::mousePressEvent(QMouseEvent* event, MapCoordF map_coord, MapWidget* widget)
@@ -82,23 +60,12 @@ bool DrawPathTool::mousePressEvent(QMouseEvent* event, MapCoordF map_coord, MapW
 		if (!draw_in_progress)
 		{
 			// Start a new path
-			drawing_symbol = is_helper_tool ? NULL : symbol_widget->getSingleSelectedSymbol();
-			if (!path_combination)
-				path_combination = new CombinedSymbol();
-			path_combination->setNumParts(is_helper_tool ? 2 : 3);
-			path_combination->setPart(0, Map::getCoveringWhiteLine());
-			path_combination->setPart(1, Map::getCoveringRedLine());
-			if (drawing_symbol)
-				path_combination->setPart(2, drawing_symbol);
-			preview_path = new PathObject(path_combination);
+			startDrawing();
 			
 			path_has_preview_point = false;
 			previous_point_is_curve_point = false;
 			
-			draw_in_progress = true;
 			updateStatusText();
-			
-			editor->setEditingInProgress(true);
 		}
 
 		// Set path point
@@ -137,17 +104,7 @@ bool DrawPathTool::mouseMoveEvent(QMouseEvent* event, MapCoordF map_coord, MapWi
 		if (!draw_in_progress)
 		{
 			// Show preview objects at this position
-			int size = (int)preview_points.size();
-			for (int i = 0; i < size; ++i)
-			{
-				if (preview_points_shown)
-					renderables.removeRenderablesOfObject(preview_points[i], false);
-				preview_points[i]->setPosition(map_coord);
-				preview_points[i]->update(true);
-				renderables.insertRenderablesOfObject(preview_points[i]);
-			}
-			preview_points_shown = true;
-			
+			setPreviewPointsPosition(map_coord);
 			setDirtyRect(map_coord);
 		}
 		else // if (draw_in_progress)
@@ -259,11 +216,6 @@ bool DrawPathTool::mouseDoubleClickEvent(QMouseEvent* event, MapCoordF map_coord
 		finishDrawing();
 	return true;
 }
-void DrawPathTool::leaveEvent(QEvent* event)
-{
-	if (!draw_in_progress)
-		editor->getMap()->clearDrawingBoundingBox();
-}
 
 bool DrawPathTool::keyPressEvent(QKeyEvent* event)
 {
@@ -295,30 +247,10 @@ bool DrawPathTool::keyPressEvent(QKeyEvent* event)
 	
 	return true;
 }
-bool DrawPathTool::keyReleaseEvent(QKeyEvent* event)
-{
-	return false;
-}
-void DrawPathTool::focusOutEvent(QFocusEvent* event)
-{
-	// Deactivate all modifiers - not always correct, but should be wrong only in very unusual cases and better than leaving the modifiers on forever
-	//space_pressed = false;
-	//updateStatusText();
-}
 
 void DrawPathTool::draw(QPainter* painter, MapWidget* widget)
 {
-	if (preview_path || preview_points_shown)
-	{
-		painter->save();
-		painter->translate(widget->width() / 2.0 + widget->getMapView()->getDragOffset().x(),
-						   widget->height() / 2.0 + widget->getMapView()->getDragOffset().y());
-		widget->getMapView()->applyTransform(painter);
-		
-		renderables.draw(painter, widget->getMapView()->calculateViewedRect(widget->viewportToView(widget->rect())), true, widget->getMapView()->calculateFinalZoomFactor(), true, 0.5f);
-		
-		painter->restore();
-	}
+	drawPreviewObjects(painter, widget);
 	
 	if (draw_in_progress)
 	{
@@ -447,62 +379,25 @@ void DrawPathTool::finishDrawing()
 	if (path_has_preview_point && !dragging)
 		preview_path->deleteCoordinate(preview_path->getCoordinateCount() - (preview_path->getPart(0).isClosed() ? 2 : 1), false);
 	
-	renderables.removeRenderablesOfObject(preview_path, false);
-	
 	if (preview_path->getCoordinateCount() < (contains_only_areas ? 3 : 2))
 	{
 		delete preview_path;
 		preview_path = NULL;
 	}
-	else if (!is_helper_tool)
-	{
-		preview_path->setSymbol(drawing_symbol, true);
-		int index = editor->getMap()->addObject(preview_path);
-		editor->getMap()->clearObjectSelection(false);
-		editor->getMap()->addObjectToSelection(preview_path, true);
-		
-		DeleteObjectsUndoStep* undo_step = new DeleteObjectsUndoStep(editor->getMap());
-		undo_step->addObject(index);
-		editor->getMap()->objectUndoManager().addNewUndoStep(undo_step);
-	}
-	editor->getMap()->clearDrawingBoundingBox();
 	
 	dragging = false;
 	draw_in_progress = false;
-	
 	updateStatusText();
-	editor->setEditingInProgress(false);
 	
-	if (is_helper_tool)
-	{
-		if (preview_path)
-		{
-			// Ugly HACK to make it possible to delete this tool as response to pathFinished
-			PathObject* temp_path = preview_path;
-			preview_path = NULL;
-			emit(pathFinished(temp_path));
-			delete temp_path;
-		}
-		else
-			emit(pathAborted());
-	}
-	else
-		preview_path = NULL;
+	DrawLineAndAreaTool::finishDrawing();
 }
 void DrawPathTool::abortDrawing()
 {
-	renderables.removeRenderablesOfObject(preview_path, false);
-	delete preview_path;
-	editor->getMap()->clearDrawingBoundingBox();
-	
-	preview_path = NULL;
 	dragging = false;
 	draw_in_progress = false;
-	
 	updateStatusText();
-	editor->setEditingInProgress(false);
 	
-	emit(pathAborted());
+	DrawLineAndAreaTool::abortDrawing();
 }
 
 void DrawPathTool::setDirtyRect(MapCoordF mouse_pos)
@@ -531,18 +426,6 @@ void DrawPathTool::setDirtyRect(MapCoordF mouse_pos)
 			editor->getMap()->clearDrawingBoundingBox();
 	}
 }
-void DrawPathTool::includePreviewRects(QRectF& rect)
-{
-	if (preview_path)
-		rectIncludeSafe(rect, preview_path->getExtent());
-	
-	if (preview_points_shown)
-	{
-		int size = (int)preview_points.size();
-		for (int i = 0; i < size; ++i)
-			rectIncludeSafe(rect, preview_points[i]->getExtent());
-	}
-}
 float DrawPathTool::calculateRotation(QPoint mouse_pos, MapCoordF mouse_pos_map)
 {
 	if (dragging && (mouse_pos - click_pos).manhattanLength() >= QApplication::startDragDistance())
@@ -568,137 +451,6 @@ void DrawPathTool::updateStatusText()
 	}
 	
 	setStatusBarText(text);
-}
-
-void DrawPathTool::updatePreviewPath()
-{
-	renderables.removeRenderablesOfObject(preview_path, false);
-	preview_path->update(true);
-	renderables.insertRenderablesOfObject(preview_path);
-}
-
-void DrawPathTool::hidePreviewPoints()
-{
-	if (!preview_points_shown)
-		return;
-	
-	int size = (int)preview_points.size();
-	for (int i = 0; i < size; ++i)
-		renderables.removeRenderablesOfObject(preview_points[i], false);
-	
-	preview_points_shown = false;
-}
-void DrawPathTool::deleteObjects()
-{
-	int size = (int)preview_points.size();
-	for (int i = 0; i < size; ++i)
-	{
-		renderables.removeRenderablesOfObject(preview_points[i], false);
-		delete preview_points[i];
-	}
-	preview_points.clear();
-	
-	size = (int)preview_point_symbols.size();
-	for (int i = 0; i < size; ++i)
-	{
-		if (!preview_point_symbols_external[i])
-			delete preview_point_symbols[i];
-	}
-	preview_point_symbols.clear();
-	preview_point_symbols_external.clear();
-
-	if (preview_path)
-	{
-		renderables.removeRenderablesOfObject(preview_path, false);
-		delete preview_path;
-		preview_path = NULL;
-	}
-	draw_in_progress = false;
-}
-void DrawPathTool::addPreviewSymbols(Symbol* symbol)
-{
-	if (symbol->getType() == Symbol::Line)
-	{
-		LineSymbol* line = reinterpret_cast<LineSymbol*>(symbol);
-		
-		bool has_main_line = line->getLineWidth() > 0 && line->getColor() != NULL;
-		bool has_border_line = line->hasBorder() && line->getBorderLineWidth() > 0 && line->getBorderColor() != NULL;
-		if (has_main_line || has_border_line)
-		{
-			if (has_main_line)
-			{
-				PointSymbol* preview = new PointSymbol();
-				preview->setInnerRadius(line->getLineWidth() / 2);
-				preview->setInnerColor(line->getColor());
-				preview_point_symbols.push_back(preview);
-				preview_point_symbols_external.push_back(false);
-			}
-			if (has_border_line)
-			{
-				PointSymbol* preview = new PointSymbol();
-				preview->setInnerRadius(line->getLineWidth() / 2 - line->getBorderLineWidth() / 2 + line->getBorderShift());
-				preview->setOuterWidth(line->getBorderLineWidth());
-				preview->setOuterColor(line->getBorderColor());
-				preview_point_symbols.push_back(preview);
-				preview_point_symbols_external.push_back(false);
-			}
-		}
-		else if (line->getMidSymbol() && !line->getMidSymbol()->isEmpty())
-		{
-			preview_point_symbols.push_back(line->getMidSymbol());
-			preview_point_symbols_external.push_back(true);
-		}
-	}
-	else if (symbol->getType() == Symbol::Combined)
-	{
-		CombinedSymbol* combined = reinterpret_cast<CombinedSymbol*>(symbol);
-		
-		int size = combined->getNumParts();
-		for (int i = 0; i < size; ++i)
-		{
-			if (combined->getPart(i))
-				addPreviewSymbols(combined->getPart(i));
-		}
-	}
-}
-
-void DrawPathTool::selectedSymbolsChanged()
-{
-	if (is_helper_tool)
-		return;
-	if (draw_in_progress)
-		abortDrawing();
-	
-	Symbol* symbol = symbol_widget->getSingleSelectedSymbol();
-	if (symbol == NULL || ((symbol->getType() & (Symbol::Line | Symbol::Area | Symbol::Combined)) == 0) || symbol->isHidden())
-	{
-		if (symbol && symbol->isHidden())
-			editor->setEditTool();
-		else
-			editor->setTool(editor->getDefaultDrawToolForSymbol(symbol));
-		return;
-	}
-
-	deleteObjects();
-	addPreviewSymbols(symbol);
-	
-	// Create objects for the symbols
-	int size = (int)preview_point_symbols.size();
-	preview_points.resize(size);
-	for (int i = 0; i < size; ++i)
-		preview_points[i] = new PointObject(preview_point_symbols[i]);
-	
-	last_used_symbol = symbol;
-}
-void DrawPathTool::symbolChanged(int pos, Symbol* new_symbol, Symbol* old_symbol)
-{
-	if (old_symbol == last_used_symbol)
-		selectedSymbolsChanged();
-}
-void DrawPathTool::symbolDeleted(int pos, Symbol* old_symbol)
-{
-	if (old_symbol == last_used_symbol)
-		editor->setEditTool();
 }
 
 #include "tool_draw_path.moc"

CMakeLists.txt
@@ -82,8 +82,10 @@ set(Mapper_SRCS
  src/paint_on_template.cpp
 
  src/tool_edit.cpp
+ src/tool_draw_line_and_area.cpp
  src/tool_draw_point.cpp
  src/tool_draw_path.cpp
+ src/tool_draw_circle.cpp
  src/tool_draw_text.cpp
  src/tool_cut.cpp
  src/tool_cut_hole.cpp

resources.qrc
@@ -10,6 +10,7 @@
   <file>images/cursor-crosshair.png</file>
   <file>images/cursor-cut.png</file>
   <file>images/cursor-delete.png</file>
+  <file>images/cursor-draw-circle.png</file>
   <file>images/cursor-draw-path.png</file>
   <file>images/cursor-draw-point.png</file>
   <file>images/cursor-draw-text.png</file>
@@ -21,6 +22,7 @@
   <file>images/cursor-rotate.png</file>
   <file>images/cut.png</file>
   <file>images/delete.png</file>
+  <file>images/draw-circle.png</file>
   <file>images/draw-path.png</file>
   <file>images/draw-point.png</file>
   <file>images/draw-text.png</file>

src/map_coord.h
@@ -277,12 +277,23 @@ public:
 		y = sin(new_angle) * len;
 	}
 	
-	/// Get a perpendicular vector pointing to the right
+	/// Replaces this vector with a perpendicular vector pointing to the right
 	inline void perpRight()
 	{
 		double x = getX();
 		setX(-getY());
 		setY(x);
+	}
+	
+	/// Scales the vector to a given length
+	inline void setLength(double length)
+	{
+		double cur_length = sqrt(getX()*getX() + getY()*getY());
+		if (cur_length < 1e-08)
+			return;
+		
+		setX(getX() * length / cur_length);
+		setY(getY() * length / cur_length);
 	}
 	
 	MapCoord toMapCoord() const

src/map_editor.cpp
@@ -37,6 +37,7 @@
 #include "symbol.h"
 #include "tool_draw_point.h"
 #include "tool_draw_path.h"
+#include "tool_draw_circle.h"
 #include "tool_draw_text.h"
 #include "tool_edit.h"
 #include "util.h"
@@ -334,6 +335,7 @@ void MapEditorController::assignKeyboardShortcuts()
 	findAction("editobjects")->setShortcut(QKeySequence("E"));
 	findAction("drawpoint")->setShortcut(QKeySequence("S"));
 	findAction("drawpath")->setShortcut(QKeySequence("P"));
+	findAction("drawcircle")->setShortcut(QKeySequence("O"));
 	findAction("drawtext")->setShortcut(QKeySequence("T"));
 	
     findAction("duplicate")->setShortcut(QKeySequence("D"));
@@ -374,6 +376,7 @@ void MapEditorController::createMenuAndToolbars()
     edit_tool_act = newCheckAction("editobjects", tr("Edit objects"), this, SLOT(editToolClicked(bool)), "tool-edit.png");
     draw_point_act = newCheckAction("drawpoint", tr("Set point objects"), this, SLOT(drawPointClicked(bool)), "draw-point.png");
     draw_path_act = newCheckAction("drawpath", tr("Draw paths"), this, SLOT(drawPathClicked(bool)), "draw-path.png");
+	draw_circle_act = newCheckAction("drawcircle", tr("Draw circles"), this, SLOT(drawCircleClicked(bool)), "draw-circle.png");
     draw_text_act = newCheckAction("drawtext", tr("Write text"), this, SLOT(drawTextClicked(bool)), "draw-text.png");
     duplicate_act = newAction("duplicate", tr("Duplicate"), this, SLOT(duplicateClicked()), "tool-duplicate.png"); // D
     switch_symbol_act = newAction("switchsymbol", tr("Switch symbol"), this, SLOT(switchSymbolClicked()), "tool-switch-symbol.png");
@@ -381,7 +384,10 @@ void MapEditorController::createMenuAndToolbars()
     switch_dashes_act = newAction("switchdashes", tr("Switch dash direction"), this, SLOT(switchDashesClicked()), "tool-switch-dashes"); // Ctrl+D
 	connect_paths_act = newAction("connectpaths", tr("Connect paths"), this, SLOT(connectPathsClicked()), "tool-connect-paths.png");
 	cut_tool_act = newCheckAction("cutobject", tr("Cut object"), this, SLOT(cutClicked(bool)), "tool-cut.png");
-	cut_hole_act = newCheckAction("cuthole", tr("Cut holes"), this, SLOT(cutHoleClicked(bool)), "tool-cut-hole.png");
+	cut_hole_act = newCheckAction("cuthole", tr("Cut free form hole"), this, SLOT(cutHoleClicked(bool)), "tool-cut-hole.png"); // cut hole using a path
+	cut_hole_circle_act = new QAction(QIcon(":/images/tool-cut-hole.png"), tr("Cut round hole"), this);
+	cut_hole_circle_act->setCheckable(true);
+	QObject::connect(cut_hole_circle_act, SIGNAL(triggered(bool)), this, SLOT(cutHoleCircleClicked(bool)));
     rotate_act = newCheckAction("rotateobjects", tr("Rotate object(s)"), this, SLOT(rotateClicked(bool)), "tool-rotate.png");
 	measure_act = newCheckAction("measure", tr("Measure lengths and areas"), this, SLOT(measureClicked(bool)), "tool-measure.png");
 
@@ -416,6 +422,7 @@ void MapEditorController::createMenuAndToolbars()
     tools_menu->addAction(edit_tool_act);
     tools_menu->addAction(draw_point_act);
     tools_menu->addAction(draw_path_act);
+	tools_menu->addAction(draw_circle_act);
     tools_menu->addAction(draw_text_act);
     tools_menu->addAction(duplicate_act);
     tools_menu->addAction(switch_symbol_act);
@@ -423,7 +430,11 @@ void MapEditorController::createMenuAndToolbars()
     tools_menu->addAction(switch_dashes_act);
 	tools_menu->addAction(connect_paths_act);
 	tools_menu->addAction(cut_tool_act);
-	tools_menu->addAction(cut_hole_act);
+	QMenu *cut_hole_menu = new QMenu(tr("Cut hole"));
+	cut_hole_menu->setIcon(QIcon(":/images/tool-cut-hole.png"));
+	cut_hole_menu->addAction(cut_hole_act);
+	cut_hole_menu->addAction(cut_hole_circle_act);
+	tools_menu->addMenu(cut_hole_menu);
 	tools_menu->addAction(rotate_act);
 	tools_menu->addAction(measure_act);
 	
@@ -473,6 +484,7 @@ void MapEditorController::createMenuAndToolbars()
 	toolbar_drawing->addAction(edit_tool_act);
     toolbar_drawing->addAction(draw_point_act);
 	toolbar_drawing->addAction(draw_path_act);
+	toolbar_drawing->addAction(draw_circle_act);
     toolbar_drawing->addAction(draw_text_act);
 
 	toolbar_drawing->addSeparator();
@@ -501,7 +513,15 @@ void MapEditorController::createMenuAndToolbars()
     toolbar_editing->addAction(switch_dashes_act);
 	toolbar_editing->addAction(connect_paths_act);
 	toolbar_editing->addAction(cut_tool_act);
-	toolbar_editing->addAction(cut_hole_act);
+	
+	QToolButton* cut_hole_button = new QToolButton();
+	cut_hole_button->setCheckable(true);
+	cut_hole_button->setToolButtonStyle(Qt::ToolButtonIconOnly);
+	cut_hole_button->setDefaultAction(cut_hole_act);
+	cut_hole_button->setPopupMode(QToolButton::MenuButtonPopup);
+	cut_hole_button->setMenu(cut_hole_menu);
+	toolbar_editing->addWidget(cut_hole_button);
+	
 	toolbar_editing->addAction(rotate_act);
 	toolbar_editing->addAction(measure_act);
 #endif
@@ -786,6 +806,8 @@ void MapEditorController::selectedSymbolsChanged()
 	draw_point_act->setStatusTip(tr("Place point objects on the map.") + (draw_point_act->isEnabled() ? "" : (" " + tr("Select a point symbol to be able to use this tool."))));
 	draw_path_act->setEnabled((type == Symbol::Line || type == Symbol::Area || type == Symbol::Combined) && !symbol->isHidden());
 	draw_path_act->setStatusTip(tr("Draw polygonal and curved lines.") + (draw_path_act->isEnabled() ? "" : (" " + tr("Select a line, area or combined symbol to be able to use this tool."))));
+	draw_circle_act->setEnabled(draw_path_act->isEnabled());
+	draw_circle_act->setStatusTip(tr("Draw circles.") + (draw_circle_act->isEnabled() ? "" : (" " + tr("Select a line, area or combined symbol to be able to use this tool."))));
 	draw_text_act->setEnabled(type == Symbol::Text && !symbol->isHidden());
 	draw_text_act->setStatusTip(tr("Write text on the map.") + (draw_text_act->isEnabled() ? "" : (" " + tr("Select a text symbol to be able to use this tool."))));
 	
@@ -870,6 +892,10 @@ void MapEditorController::drawPointClicked(bool checked)
 void MapEditorController::drawPathClicked(bool checked)
 {
 	setTool(checked ? new DrawPathTool(this, draw_path_act, symbol_widget, true) : NULL);
+}
+void MapEditorController::drawCircleClicked(bool checked)
+{
+	setTool(checked ? new DrawCircleTool(this, draw_circle_act, symbol_widget) : NULL);
 }
 void MapEditorController::drawTextClicked(bool checked)
 {
@@ -1099,7 +1125,11 @@ void MapEditorController::cutClicked(bool checked)
 }
 void MapEditorController::cutHoleClicked(bool checked)
 {
-	setTool(checked ? new CutHoleTool(this, cut_hole_act) : NULL);
+	setTool(checked ? new CutHoleTool(this, cut_hole_act, PathObject::Path) : NULL);
+}
+void MapEditorController::cutHoleCircleClicked(bool checked)
+{
+	setTool(checked ? new CutHoleTool(this, cut_hole_circle_act, PathObject::Circle) : NULL);
 }
 void MapEditorController::rotateClicked(bool checked)
 {

src/map_editor.h
@@ -125,6 +125,7 @@ public slots:
 	void editToolClicked(bool checked);
 	void drawPointClicked(bool checked);
 	void drawPathClicked(bool checked);
+	void drawCircleClicked(bool checked);
 	void drawTextClicked(bool checked);
 	
 	void duplicateClicked();
@@ -134,6 +135,7 @@ public slots:
 	void connectPathsClicked();
 	void cutClicked(bool checked);
 	void cutHoleClicked(bool checked);
+	void cutHoleCircleClicked(bool checked);
 	void rotateClicked(bool checked);
 	void measureClicked(bool checked);
 	
@@ -195,6 +197,7 @@ private:
 	QAction* edit_tool_act;
 	QAction* draw_point_act;
 	QAction* draw_path_act;
+	QAction* draw_circle_act;
 	QAction* draw_text_act;
 	
 	QAction* duplicate_act;
@@ -204,6 +207,7 @@ private:
 	QAction* connect_paths_act;
 	QAction* cut_tool_act;
 	QAction* cut_hole_act;
+	QAction* cut_hole_circle_act;
 	QAction* rotate_act;
 	QAction* measure_act;
 	EditorDockWidget* measure_dock_widget;

commit 07ffdb0ffb875008a03c766ec3649c21e4e0ad8d

Fixed crash when finishing path after only one point (introduced in 699e0b4 Implement the circle drawing tool)

src/tool_draw_path.cpp
@@ -381,6 +381,7 @@ void DrawPathTool::finishDrawing()
 	
 	if (preview_path->getCoordinateCount() < (contains_only_areas ? 3 : 2))
 	{
+		renderables.removeRenderablesOfObject(preview_path, false);
 		delete preview_path;
 		preview_path = NULL;
 	}