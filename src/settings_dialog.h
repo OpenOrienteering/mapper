90fef6ef00da8d11a919efa8f10dd49ac5aee9cf
Jan Dalheimer <jan@dalheimer.de>
2012-05-01 17:18:51 +0200

    Added licenses to the new files

--- src/settings_dialog.h
+++ src/settings_dialog.h
@@ -1,3 +1,22 @@
+/*
+ *    Copyright 2012 Jan Dalheimer
+ *
+ *    This file is part of OpenOrienteering.
+ *
+ *    OpenOrienteering is free software: you can redistribute it and/or modify
+ *    it under the terms of the GNU General Public License as published by
+ *    the Free Software Foundation, either version 3 of the License, or
+ *    (at your option) any later version.
+ *
+ *    OpenOrienteering is distributed in the hope that it will be useful,
+ *    but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *    GNU General Public License for more details.
+ *
+ *    You should have received a copy of the GNU General Public License
+ *    along with OpenOrienteering.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
 #ifndef SETTINGS_DIALOG_H
 #define SETTINGS_DIALOG_H
 
@@ -13,7 +32,7 @@ class SettingsPage : public QWidget
 {
 	Q_OBJECT
 public:
-	SettingsPage(MainWindow* main_window, QWidget *parent = 0) : QWidget(parent), main_window(main_window){}
+	SettingsPage(MainWindow* main_window, QWidget* parent = 0) : QWidget(parent), main_window(main_window){}
 	virtual void cancel() = 0;
 	virtual void apply();
 	virtual void ok(){ this->apply(); }
