b3f58fe40fe9dd2e582b11d6b791b745acd872db
Thomas Schoeps <tom.schoeps@googlemail.com>
2012-05-12 15:53:04 +0200

    Improved first tip of the day and the 'ready to draw' help text

--- src/map_widget.cpp
+++ src/map_widget.cpp
@@ -515,7 +515,7 @@ void MapWidget::paintEvent(QPaintEvent* event)
 	else if (show_help && view && view->getMap()->getNumSymbols() == 0)
 		showHelpMessage(&painter, tr("No symbols!\n\nNow define some symbols:\nRight-click in the symbol bar\nand select \"New symbol\"\nto create one."));
 	else if (show_help && view && view->getMap()->getNumTemplates() == 0 && view->getMap()->getNumObjects() == 0)	// No templates or objects defined?
-		showHelpMessage(&painter, tr("Ready to draw!\n\nStart drawing or load a base map.\nTo load a base map, click\nTemplates -> Open template..."));
+		showHelpMessage(&painter, tr("Ready to draw!\n\nStart drawing or load a base map.\nTo load a base map, click\nTemplates -> Open template...") + "\n\n" + tr("Hint: Hold the middle mouse button to drag the map,\nzoom using the mouse wheel, if available."));
 	else if (view)
 	{
 		// Update all dirty caches
