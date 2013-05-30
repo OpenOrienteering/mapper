e780f04c1698d34363879e05e88de2c006fc80dc
Kai Pastor <dg0yt@darc.de>
2013-05-30 15:29:12 +0200

    TemplateWidget: Make updateButtons() an explicit method.

--- src/template_dock_widget.h
+++ src/template_dock_widget.h
@@ -69,7 +69,7 @@ protected slots:
 	void showHelp();
 	
 	void cellChange(int row, int column);
-	void selectionChanged(const QItemSelection& selected, const QItemSelection& deselected);
+	void updateButtons();
 	void currentCellChange(int current_row, int current_column, int previous_row, int previous_column);
 	void cellDoubleClick(int row, int column);
 	void updateDeleteButtonText();
