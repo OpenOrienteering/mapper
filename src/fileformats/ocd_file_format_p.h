75b3a4d0b9fa14d4e4282e97373c83e8ca78c76a
Kai Pastor <dg0yt@darc.de>
2013-08-15 09:07:09 +0200

    OCD import fixes
    - Insert templates in the correct order.
    - OcFileImport::convertOcdPoint(): Allow build-time selection between code
      relying on the implementation of operator>>() as arithmetic shift and an
      equivalent implementation not making this assumption. Fix the previous
      implementation of the code for the second option.

--- src/fileformats/ocd_file_format_p.h
+++ src/fileformats/ocd_file_format_p.h
@@ -296,9 +296,17 @@ QString OcdFileImport::convertOcdString(const QChar* src) const
 inline
 MapCoord OcdFileImport::convertOcdPoint(const Ocd::OcdPoint32& ocd_point) const
 {
-	// Do not use operator>>() on the signed integers ocd_point.x/y,
-	// and don't use rounding!
-	return MapCoord::fromRaw(10 * (ocd_point.x / 256), -10 * (ocd_point.y / 256));
+	// Behavior of operator>>() on negative integers is implementation defined.
+	// Define SHIFT_OPERATOR_IS_BINARY if the compiler does not implement
+	// operator>>() as arithmetic shift (maintaining the sign).
+#if defined(SHIFT_OPERATOR_IS_BINARY)
+	const qint64 x = (ocd_point.x > 0) ? (ocd_point.x >> 8) : (-1 - ((-1-ocd_point.x) >> 8));
+	const qint64 y = (ocd_point.y > 0) ? (ocd_point.y >> 8) : (-1 - ((-1-ocd_point.y) >> 8));
+	return MapCoord::fromRaw(x * 10, y * -10);
+#else // shift operator is arithmetic
+	Q_ASSERT( (-3 >> 1) == -2 );
+	return MapCoord::fromRaw(qint64(ocd_point.x >> 8) * 10, qint64(ocd_point.x >> 8) * -10);
+#endif
 }
 
 inline
