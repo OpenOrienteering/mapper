86d0332800e1a67218fb6af05b0263e2176b22cd
Kai Pastor <dg0yt@darc.de>
2014-11-22 19:36:38 +0100

    translations: Add plurals-only English "translation"

---
+++ translations/OpenOrienteering_en.ts
@@ -0,0 +1,38 @@
+<?xml version="1.0" encoding="utf-8"?>
+<!DOCTYPE TS>
+<TS version="2.1" language="en_US">
+<context>
+    <name>AutosaveDialog</name>
+    <message numerus="yes">
+        <location filename="../src/gui/autosave_dialog.cpp" line="37"/>
+        <location filename="../src/gui/autosave_dialog.cpp" line="43"/>
+        <source>%n bytes</source>
+        <translation>
+            <numerusform>%n byte</numerusform>
+            <numerusform>%n bytes</numerusform>
+        </translation>
+    </message>
+</context>
+<context>
+    <name>OCAD8FileImport</name>
+    <message numerus="yes">
+        <location filename="../src/file_format_ocad8.cpp" line="142"/>
+        <source>%n color separation(s) were skipped, reason: Import disabled.</source>
+        <translation>
+            <numerusform>%n color separation was skipped, reason: Import disabled.</numerusform>
+            <numerusform>%n color separations were skipped, reason: Import disabled.</numerusform>
+        </translation>
+    </message>
+</context>
+<context>
+    <name>TemplateTrack</name>
+    <message numerus="yes">
+        <location filename="../src/template_track.cpp" line="458"/>
+        <source>%n path object(s) could not be imported (reason: missing coordinates).</source>
+        <translation>
+            <numerusform>%n path object could not be imported (reason: missing coordinates).</numerusform>
+            <numerusform>%n path objects could not be imported (reason: missing coordinates).</numerusform>
+        </translation>
+    </message>
+</context>
+</TS>
