5858d5a15990c754b401bc2af401b05e958f6304
Kai Pastor <dg0yt@darc.de>
2015-01-29 08:45:10 +0100

    MapEditorController: Terminate editing on detach()
    Fixes crash on closing file while editing is in progress.

--- src/map_editor.cpp
+++ src/map_editor.cpp
@@ -1,6 +1,6 @@
 /*
  *    Copyright 2012, 2013 Thomas Schöps
- *    Copyright 2012, 2013, 2014 Kai Pastor
+ *    Copyright 2012-2015 Kai Pastor
  *
  *    This file is part of OpenOrienteering.
  *
@@ -1273,6 +1273,10 @@ void MapEditorController::createMobileGUI()
 
 void MapEditorController::detach()
 {
+	// Terminate all editing
+	setTool(nullptr);
+	setOverrideTool(nullptr);
+	
 	if (!mobile_mode)
 		saveWindowState();
 	being_destructed = true;
