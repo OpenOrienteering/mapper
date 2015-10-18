a5fcbc2c0d4384933aeea77402607881f624e29c
Kai Pastor <dg0yt@darc.de>
2015-10-18 20:39:04 +0200

    src/printsupport: Build printsupport library
    Provides a customized PDF engine and printer.

---
+++ src/printsupport/advanced_pdf_printer.cpp
@@ -0,0 +1,50 @@
+/*
+ *    Copyright 2015 Kai Pastor
+ *
+ *    This file is part of OpenOrienteering.
+ *
+ *    OpenOrienteering is free software: you can redistribute it and/or modify
+ *    it under the terms of the GNU General Public License as published by
+ *    the Free Software Foundation, either version 3 of the License, or
+ *    (at your option) any later version.
+ *
+ *    OpenOrienteering is distributed in the hope that it will be useful,
+ *    but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *    GNU General Public License for more details.
+ *
+ *    You should have received a copy of the GNU General Public License
+ *    along with OpenOrienteering.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+#include "advanced_pdf_printer.h"
+
+#include <advanced_pdf_p.h>
+#include <printengine_advanced_pdf_p.h>
+
+
+AdvancedPdfPrinter::AdvancedPdfPrinter(QPrinter::PrinterMode mode)
+ : QPrinter{ mode }
+ , engine{ new AdvancedPdfPrintEngine{ mode} }
+{
+	setOutputFormat(PdfFormat);
+	setEngines(engine.get(), engine.get());
+}
+
+AdvancedPdfPrinter::AdvancedPdfPrinter(const QPrinterInfo& printer, QPrinter::PrinterMode mode)
+ : QPrinter{ printer, mode }
+ , engine{ new AdvancedPdfPrintEngine{ mode} }
+{
+	setOutputFormat(PdfFormat);
+	setEngines(engine.get(), engine.get());
+}
+
+AdvancedPdfPrinter::~AdvancedPdfPrinter()
+{
+	// Nothing, not inlined
+}
+
+QPaintEngine::Type AdvancedPdfPrinter::paintEngineType()
+{
+	return AdvancedPdfEngine::PaintEngineType;
+}
---
+++ src/printsupport/advanced_pdf_printer.h
@@ -0,0 +1,50 @@
+/*
+ *    Copyright 2015 Kai Pastor
+ *
+ *    This file is part of OpenOrienteering.
+ *
+ *    OpenOrienteering is free software: you can redistribute it and/or modify
+ *    it under the terms of the GNU General Public License as published by
+ *    the Free Software Foundation, either version 3 of the License, or
+ *    (at your option) any later version.
+ *
+ *    OpenOrienteering is distributed in the hope that it will be useful,
+ *    but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *    GNU General Public License for more details.
+ *
+ *    You should have received a copy of the GNU General Public License
+ *    along with OpenOrienteering.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+#ifndef OPENORIENTEERING_ADVANCED_PDF_H
+#define OPENORIENTEERING_ADVANCED_PDF_H
+
+#include <memory>
+
+#include <QPaintEngine>
+#include <QPrinter>
+
+
+class AdvancedPdfPrintEngine;
+
+class AdvancedPdfPrinter : public QPrinter
+{
+public:
+	/** Creates a PdfFormat printer based on AdvancedPdfPrintEngine. */
+	explicit AdvancedPdfPrinter(PrinterMode mode = ScreenResolution);
+	
+	/** Creates a PdfFormat printer based on AdvancedPdfPrintEngine. */
+	explicit AdvancedPdfPrinter(const QPrinterInfo& printer, PrinterMode mode = ScreenResolution);
+	
+	/** Destructor. */
+	~AdvancedPdfPrinter();
+	
+	/** Returns the paint engine type which is used for advanced pdf generation. */
+	static QPaintEngine::Type paintEngineType();
+	
+private:
+	std::unique_ptr<AdvancedPdfPrintEngine> engine;
+};
+
+#endif
