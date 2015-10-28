f96b375c433552a70f896818f373e6d631fc169a
Kai Pastor <dg0yt@darc.de>
2015-10-28 09:19:50 +0100

    PrintTool: Cleanup in draw()

--- src/gui/print_tool.cpp
+++ src/gui/print_tool.cpp
@@ -154,28 +154,7 @@ void PrintTool::draw(QPainter* painter, MapWidget* widget)
 	QColor top_left_margin_color(255, 0, 0, 160);
 	QColor bottom_right_margin_color(255, 128, 128, 160);
 	painter->setBrush(Qt::NoBrush);
-#if 0
-	Q_FOREACH(qreal hpos, map_printer->horizontalPagePositions())
-	{
-		qreal x_pos = widget->mapToViewport(MapCoordF(hpos, 0)).x();
-		painter->setPen(top_left_margin_color);
-		painter->drawLine(x_pos, outer_rect.top(), x_pos, outer_rect.bottom());
-		
-		x_pos += page_size.width();
-		painter->setPen(bottom_right_margin_color);
-		painter->drawLine(x_pos, outer_rect.top(), x_pos, outer_rect.bottom());
-	}
-	Q_FOREACH(qreal vpos, map_printer->verticalPagePositions())
-	{
-		qreal y_pos = widget->mapToViewport(MapCoordF(0, vpos)).y();
-		painter->setPen(top_left_margin_color);
-		painter->drawLine(outer_rect.left(), y_pos, outer_rect.right(), y_pos);
-		
-		y_pos += page_size.height();
-		painter->setPen(bottom_right_margin_color);
-		painter->drawLine(outer_rect.left(), y_pos, outer_rect.right(), y_pos);
-	}
-#else
+
 	// The relative length of the page dimensions to be actual drawn.
 	QSizeF drawing_size(page_size * scale_adjustment);
 	if (map_printer->horizontalPagePositions().size() > 1)
@@ -193,18 +172,21 @@ void PrintTool::draw(QPainter* painter, MapWidget* widget)
 		  map_printer->getPageFormat().page_rect.height() );
 	}
 	
-	std::vector<qreal>::const_iterator hpos = map_printer->horizontalPagePositions().begin();
-	std::vector<qreal>::const_iterator hend = map_printer->horizontalPagePositions().end();
-	for (int h = 0; hpos != hend; ++hpos, ++h)
+	int h = 0;
+	for (auto hpos : map_printer->horizontalPagePositions())
 	{
-		std::vector<qreal>::const_iterator vpos = map_printer->verticalPagePositions().begin();
-		std::vector<qreal>::const_iterator vend = map_printer->verticalPagePositions().end();
-		for (int v = 0; vpos != vend; ++vpos, ++v)
+		++h;
+		if (h > 100) // Don't visualize too many pages.
+			break;
+		
+		int v = 0;
+		for (auto vpos : map_printer->verticalPagePositions())
 		{
+			++v;
 			if (h+v > 100) // Don't visualize too many pages.
-				continue;
+				break;
 			
-			QPointF pos = widget->mapToViewport(MapCoordF(*hpos, *vpos));
+			QPointF pos = widget->mapToViewport(MapCoordF(hpos, vpos));
 			painter->setPen(top_left_margin_color);
 			// Left vertical line
 			painter->drawLine(pos.x(), pos.y(), pos.x(), pos.y()+drawing_size.height());
@@ -226,7 +208,6 @@ void PrintTool::draw(QPainter* painter, MapWidget* widget)
 	painter->setPen(bottom_right_margin_color);
 	painter->drawLine(outer_rect.right(), outer_rect.top(), outer_rect.right(), outer_rect.bottom());
 	painter->drawLine(outer_rect.left(), outer_rect.bottom(), outer_rect.right(), outer_rect.bottom());
-#endif
 	
 	QRectF print_area_f(print_area);
 	QPen marker(Qt::red);
