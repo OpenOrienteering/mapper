97ba548a101fa9e794da65f850d6db628f883997
Marek Karm <marek.karm@gmail.com>
2016-07-21 10:57:00 +0000

    Added translation using Weblate (Estonian)

---
+++ translations/OpenOrienteering_et.ts
@@ -0,0 +1,41 @@
+<?xml version='1.0' encoding='utf-8'?>
+<!DOCTYPE TS>
+<TS version="2.1" language="et">
+<context>
+    <name>AutosaveDialog</name>
+    <message numerus="yes">
+        <location filename="../src/gui/autosave_dialog.cpp" line="45"></location>
+        <location filename="../src/gui/autosave_dialog.cpp" line="51"></location>
+        <source>%n bytes</source>
+        <translation type="unfinished"><numerusform></numerusform>
+        <numerusform></numerusform>
+        </translation></message>
+</context>
+<context>
+    <name>Importer</name>
+    <message numerus="yes">
+        <location filename="../src/file_import_export.cpp" line="93"></location>
+        <source>Dropped %n irregular object(s).</source>
+        <translation type="unfinished"><numerusform></numerusform>
+        <numerusform></numerusform>
+        </translation></message>
+</context>
+<context>
+    <name>OCAD8FileImport</name>
+    <message numerus="yes">
+        <location filename="../src/file_format_ocad8.cpp" line="145"></location>
+        <source>%n color separation(s) were skipped, reason: Import disabled.</source>
+        <translation type="unfinished"><numerusform></numerusform>
+        <numerusform></numerusform>
+        </translation></message>
+</context>
+<context>
+    <name>TemplateTrack</name>
+    <message numerus="yes">
+        <location filename="../src/template_track.cpp" line="479"></location>
+        <source>%n path object(s) could not be imported (reason: missing coordinates).</source>
+        <translation type="unfinished"><numerusform></numerusform>
+        <numerusform></numerusform>
+        </translation></message>
+</context>
+</TS>
