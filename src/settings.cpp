05d623762bf4fcdfc9a7ceca9dabae189011751f
Kai Pastor <dg0yt@darc.de>
2017-05-07 14:31:20 +0200

    Settings: Enforce another migration
    Benefit: Unset legacy compatibility settings

--- src/settings.cpp
+++ src/settings.cpp
@@ -98,7 +98,7 @@ Settings::Settings()
 	static bool migration_checked = false;
 	if (!migration_checked)
 	{
-		QVariant current_version { QLatin1String("0.5.9") };
+		QVariant current_version { QLatin1String("0.7") };
 		QSettings settings;
 		if (settings.value(QString::fromLatin1("version")) != current_version)
 		{
