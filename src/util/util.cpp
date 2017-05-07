32cdd026273f40f7ba9f6bd66036ef2b5768caaa
Kai Pastor <dg0yt@darc.de>
2017-05-07 17:07:52 +0200

    doc/manual: Deploy to subdir on Android
    Avoid mixing of licensing and manual files in one directory.
    In addition, fix the construction of manual URLs so that images and links
    are loaded correctly.

--- src/util/util.cpp
+++ src/util/util.cpp
@@ -233,7 +233,7 @@ void showHelp(QWidget* dialog_parent, const char* file_and_anchor_latin1)
 void showHelp(QWidget* dialog_parent, QString filename)
 {
 #if defined(Q_OS_ANDROID)
-	const QString manual_path = QLatin1String("doc:") + filename;
+	const QString manual_path = QLatin1String("doc:/manual/") + filename;
 	const QUrl help_url = QUrl::fromLocalFile(manual_path);
 	TextBrowserDialog help_dialog(help_url, dialog_parent);
 	help_dialog.exec();
