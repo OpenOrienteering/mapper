OpenOrienteering/mapper

commit 9bb9f2542b59cc66b1824765496af53f0183b2c6

Map, MapPart: Review object operation API
Use explicit std::function instead of functor class template, and move
implementation from header to cpp file which should reduce template bloat.
Allow operations which take an Object* argument, only.
Both changes together make it possible to directly use pointers to Object
member functions (e.g. &Object::update, &Object::forceUpdate).
Use void instead of bool for return type. It was ignored most of the time.
Leverage <algorithm> and C++11.

src/tools/cutout_tool.cpp
@@ -175,31 +175,30 @@ void CutoutTool::dragFinish()
 
 struct PhysicalCutoutOperation
 {
-	inline PhysicalCutoutOperation(Map* map, PathObject* cutout_object, bool cut_away)
-	: map(map), cutout_object(cutout_object), cut_away(cut_away)
+	PhysicalCutoutOperation(Map* map, PathObject* cutout_object, bool cut_away)
+	: map(map)
+	, cutout_object(cutout_object)
+	, cut_away(cut_away)
 	{
 		add_step = new AddObjectsUndoStep(map);
 		delete_step = new DeleteObjectsUndoStep(map);
 	}
 	
-	inline bool operator()(Object* object, MapPart* part, int object_index)
+	void operator()(Object* object)
 	{
-		Q_UNUSED(part);
-		Q_UNUSED(object_index);
-		
 		// If there is a selection, only clip selected objects
 		if (map->getNumSelectedObjects() > 0 && !map->isObjectSelected(object))
-			return true;
+			return;
 		// Don't clip object itself
 		if (object == cutout_object)
-			return true;
+			return;
 		
 		// Early out
 		if (!object->getExtent().intersects(cutout_object->getExtent()))
 		{
 			if (!cut_away)
 				add_step->addObject(object, object);
-			return true;
+			return;
 		}
 		
 		if (object->getType() == Object::Point ||
@@ -220,7 +219,7 @@ struct PhysicalCutoutOperation
 				in_objects.push_back(object->asPath());
 				BooleanTool::PathObjects out_objects;
 				if (!boolean_tool.executeForObjects(object->asPath(), in_objects, out_objects))
-					return true;
+					return;
 				
 				add_step->addObject(object, object);
 				new_objects.insert(new_objects.end(), out_objects.begin(), out_objects.end());
@@ -237,7 +236,7 @@ struct PhysicalCutoutOperation
 			}
 		}
 		
-		return true;
+		return;
 	}
 	
 	UndoStep* finish()

src/core/map.cpp
@@ -1097,7 +1097,7 @@ void Map::drawTemplates(QPainter* painter, QRectF bounding_box, int first_templa
 void Map::updateObjects()
 {
 	// TODO: It maybe would be better if the objects entered themselves into a separate list when they get dirty so not all objects have to be traversed here
-	applyOnAllObjects(ObjectOp::Update());
+	applyOnAllObjects(&Object::update);
 }
 
 void Map::removeRenderablesOfObject(const Object* object, bool mark_area_as_dirty)
@@ -2397,6 +2397,43 @@ int Map::countObjectsInRect(QRectF map_coord_rect, bool include_hidden_objects)
 	return count;
 }
 
+
+
+bool Map::existsObject(const std::function<bool (const Object*)>& condition) const
+{
+	return std::any_of(begin(parts), end(parts), [&condition](auto part) { return part->existsObject(condition); });
+}
+
+
+void Map::applyOnMatchingObjects(const std::function<void (Object*)>& operation, const std::function<bool (const Object*)>& condition)
+{
+	for (auto part : parts)
+		part->applyOnMatchingObjects(operation, condition);
+}
+
+
+void Map::applyOnMatchingObjects(const std::function<void (Object*, MapPart*, int)>& operation, const std::function<bool (const Object*)>& condition)
+{
+	for (auto part : parts)
+		part->applyOnMatchingObjects(operation, condition);
+}
+
+
+void Map::applyOnAllObjects(const std::function<void (Object*)>& operation)
+{
+	for (auto part : parts)
+		part->applyOnAllObjects(operation);
+}
+
+
+void Map::applyOnAllObjects(const std::function<void (Object*, MapPart*, int)>& operation)
+{
+	for (auto part : parts)
+		part->applyOnAllObjects(operation);
+}
+
+
+
 void Map::scaleAllObjects(double factor, const MapCoord& scaling_center)
 {
 	applyOnAllObjects(ObjectOp::Scale{factor, MapCoordF{scaling_center}});
@@ -2409,12 +2446,12 @@ void Map::rotateAllObjects(double rotation, const MapCoord& center)
 
 void Map::updateAllObjects()
 {
-	applyOnAllObjects(ObjectOp::ForceUpdate());
+	applyOnAllObjects(&Object::forceUpdate);
 }
 
 void Map::updateAllObjectsWithSymbol(const Symbol* symbol)
 {
-	applyOnMatchingObjects(ObjectOp::ForceUpdate(), ObjectOp::HasSymbol{symbol});
+	applyOnMatchingObjects(&Object::forceUpdate, ObjectOp::HasSymbol{symbol});
 }
 
 void Map::changeSymbolForAllObjects(const Symbol* old_symbol, const Symbol* new_symbol)

src/core/map.h
@@ -24,6 +24,7 @@
 
 #include <algorithm>
 #include <cstddef>
+#include <functional>
 #include <set>
 #include <vector>
 
@@ -34,7 +35,7 @@
 #include <QMetaType>
 #include <QObject>
 #include <QPointer>
-#include <QRect>
+#include <QRectF>
 #include <QScopedPointer>
 #include <QSharedData>
 #include <QString>
@@ -47,10 +48,10 @@
 QT_BEGIN_NAMESPACE
 class QIODevice;
 class QPainter;
-class QRectF;
 class QTransform;
 class QTranslator;
 class QWidget;
+// IWYU pragma: no_forward_declare QRectF
 QT_END_NAMESPACE
 
 class CombinedSymbol;
@@ -907,29 +908,34 @@ public:
 	 */
 	int countObjectsInRect(QRectF map_coord_rect, bool include_hidden_objects);
 	
+	
 	/**
-	 * Applies a condition on all objects (until the first match is found).
+	 * Applies a condition on all objects until the first match is found.
 	 * 
 	 * @return True if there is an object matching the condition, false otherwise.
 	 */
-	template<typename Condition>
-	bool existsObject(const Condition& condition) const;
+	bool existsObject(const std::function<bool (const Object*)>& condition) const;
 	
 	/**
 	 * Applies an operation on all objects which match a particular condition.
-	 * 
-	 * @return False if the operation fails for any matching object, true otherwise.
 	 */
-	template<typename Operation, typename Condition>
-	bool applyOnMatchingObjects(const Operation& operation, const Condition& condition);
+	void applyOnMatchingObjects(const std::function<void (Object*)>& operation, const std::function<bool (const Object*)>& condition);
+	
+	/**
+	 * Applies an operation on all objects which match a particular condition.
+	 */
+	void applyOnMatchingObjects(const std::function<void (Object*, MapPart*, int)>& operation, const std::function<bool (const Object*)>& condition);
 	
 	/**
 	 * Applies an operation on all objects.
-	 * 
-	 * @return False if the operation fails for any object, true otherwise.
 	 */
-	template<typename Operation>
-	bool applyOnAllObjects(const Operation& operation);
+	void applyOnAllObjects(const std::function<void (Object*)>& operation);
+	
+	/**
+	 * Applies an operation on all objects.
+	 */
+	void applyOnAllObjects(const std::function<void (Object*, MapPart*, int)>& operation);
+	
 	
 	/** Scales all objects by the given factor. */
 	void scaleAllObjects(double factor, const MapCoord& scaling_center);
@@ -1719,33 +1725,6 @@ inline
 Map::ObjectSelection::const_iterator Map::selectedObjectsEnd() const
 {
 	return object_selection.cend();
-}
-
-template<typename Condition>
-bool Map::existsObject(const Condition& condition) const
-{
-	for (const MapPart* part : parts)
-		if (part->existsObject<Condition>(condition))
-			return true;
-	return false;
-}
-
-template<typename Operation, typename Condition>
-bool Map::applyOnMatchingObjects(const Operation& operation, const Condition& condition)
-{
-	bool result = true;
-	for (MapPart* part : parts)
-		result &= part->applyOnMatchingObjects(operation, condition);
-	return result;
-}
-
-template<typename Operation>
-bool Map::applyOnAllObjects(const Operation& operation)
-{
-	bool result = true;
-	for (MapPart* part : parts)
-		result &= part->applyOnAllObjects(operation);
-	return result;
 }
 
 inline

src/core/map_part.cpp
@@ -21,6 +21,9 @@
 
 #include "map_part.h"
 
+#include <algorithm>
+#include <iterator>
+
 #include <QIODevice>
 #include <QObject>
 #include <QStringRef>
@@ -317,4 +320,48 @@ QRectF MapPart::calculateExtent(bool include_helper_symbols) const
 		}
 	}
 	return rect;
+}
+
+
+
+bool MapPart::existsObject(const std::function<bool(const Object*)>& condition) const
+{
+	return std::any_of(begin(objects), end(objects), condition);
+}
+
+
+void MapPart::applyOnMatchingObjects(const std::function<void (Object*)>& operation, const std::function<bool (const Object*)>& condition)
+{
+	std::for_each(objects.rbegin(), objects.rend(), [&operation, &condition](auto object) {
+		if (condition(object))
+			operation(object);
+	});
+}
+
+
+void MapPart::applyOnMatchingObjects(const std::function<void (Object*, MapPart*, int)>& operation, const std::function<bool (const Object*)>& condition)
+{
+	for (auto i = objects.size(); i > 0; )
+	{
+		--i;
+		Object* const object = objects[i];
+		if (condition(object))
+			operation(object, this, int(i));
+	}
+}
+
+
+void MapPart::applyOnAllObjects(const std::function<void (Object*)>& operation)
+{
+	std::for_each(objects.rbegin(), objects.rend(), operation);
+}
+
+
+void MapPart::applyOnAllObjects(const std::function<void (Object*, MapPart*, int)>& operation)
+{
+	for (auto i = objects.size(); i > 0; )
+	{
+		--i;
+		operation(objects[i], this, int(i));
+	}
 }

src/core/map_part.h
@@ -23,6 +23,7 @@
 #define OPENORIENTEERING_MAP_PART_H
 
 #include <cstddef>
+#include <functional>
 #include <vector>
 #include <utility>
 
@@ -209,29 +210,32 @@ public:
 	 * 
 	 * @return True if there is an object matching the condition, false otherwise.
 	 */
-	template<typename Condition>
-	bool existsObject(const Condition& condition) const;
+	bool existsObject(const std::function<bool (const Object*)>& condition) const;
+	
+	/**
+	 * @copybrief   Map::applyOnMatchingObjects()
+	 * @copydetails Map::applyOnMatchingObjects()
+	 */
+	void applyOnMatchingObjects(const std::function<void (Object*)>& operation, const std::function<bool (const Object*)>& condition);
+	
+	/**
+	 * @copybrief   Map::applyOnMatchingObjects()
+	 * @copydetails Map::applyOnMatchingObjects()
+	 */
+	void applyOnMatchingObjects(const std::function<void (Object*, MapPart*, int)>& operation, const std::function<bool (const Object*)>& condition);
 	
 	/**
 	 * @copybrief   Map::applyOnAllObjects()
 	 * @copydetails Map::applyOnAllObjects()
 	 */
-	template<typename Operation, typename Condition>
-	bool applyOnMatchingObjects(const Operation& operation, const Condition& condition);
+	void applyOnAllObjects(const std::function<void (Object*)>& operation);
 	
 	/**
 	 * @copybrief   Map::applyOnAllObjects()
 	 * @copydetails Map::applyOnAllObjects()
 	 */
-	template<typename Operation>
-	bool applyOnAllObjects(const Operation& operation);
+	void applyOnAllObjects(const std::function<void (Object*, MapPart*, int)>& operation);
 	
-	/**
-	 * @copybrief   Map::applyOnAllObjects()
-	 * @copydetails Map::applyOnAllObjects()
-	 */
-	template<typename Operation>
-	bool applyOnAllObjects(Operation& operation);
 	
 private:
 	typedef std::vector<Object*> ObjectList;
@@ -269,62 +273,5 @@ const Object* MapPart::getObject(int i) const
 	return objects[std::size_t(i)];
 }
 
-template<typename Condition>
-bool MapPart::existsObject(const Condition& condition) const
-{
-	for (const auto* object : objects)
-	{
-		if (condition(object))
-			return true;
-	}
-	return false;
-}
-
-template<typename Operation, typename Condition>
-bool MapPart::applyOnMatchingObjects(const Operation& operation, const Condition& condition)
-{
-	bool result = true;
-	if (!objects.empty())
-	{
-		for (auto i = objects.size(); i > 0; )
-		{
-			--i;
-			Object* const object = objects[i];
-			if (condition(object))
-				result &= operation(object, this, i);
-		}
-	}
-	return result;
-}
-
-template<typename Operation>
-bool MapPart::applyOnAllObjects(const Operation& operation)
-{
-	bool result = true;
-	if (!objects.empty())
-	{
-		for (auto i = objects.size(); i > 0; )
-		{
-			--i;
-			result &= operation(objects[i], this, i);
-		}
-	}
-	return result;
-}
-
-template<typename Operation>
-bool MapPart::applyOnAllObjects(Operation& operation)
-{
-	bool result = true;
-	if (!objects.empty())
-	{
-		for (auto i = objects.size(); i > 0; )
-		{
-			--i;
-			result &= operation(objects[i], this, i);
-		}
-	}
-	return result;
-}
 
 #endif

src/core/objects/object_operations.h
@@ -34,15 +34,6 @@ namespace ObjectOp
 {
 	// Conditions
 	
-	/** Returns true for all objects. */
-	struct NoCondition
-	{
-		bool operator()(const Object*) const noexcept
-		{
-			return true;
-		}
-	};
-	
 	/** Returns true for objects with the given symbol. */
 	struct HasSymbol
 	{
@@ -85,11 +76,10 @@ namespace ObjectOp
 		double factor;
 		MapCoordF center;
 		
-		bool operator()(Object* object, MapPart*, int) const
+		void operator()(Object* object) const
 		{
 			object->scale(center, factor);
 			object->update();
-			return true;
 		}
 	};
 	
@@ -99,32 +89,10 @@ namespace ObjectOp
 		double angle;
 		MapCoordF center;
 		
-		bool operator()(Object* object, MapPart*, int) const
+		void operator()(Object* object) const
 		{
 			object->rotateAround(center, angle);
 			object->update();
-			return true;
-		}
-	};
-	
-	/** Calls update() on the objects. */
-	struct Update
-	{
-		bool operator()(Object* object, MapPart*, int) const
-		{
-			object->update();
-			return true;
-		}
-	};
-	
-	/** Calls update() on the objects. */
-	struct ForceUpdate
-	{
-		bool operator()(Object* object, MapPart*, int) const
-		{
-			object->setOutputDirty();
-			object->update();
-			return true;
 		}
 	};
 	
@@ -136,37 +104,21 @@ namespace ObjectOp
 	{
 		const Symbol* new_symbol;
 		
-		bool operator()(Object* object, MapPart* part, int object_index) const
+		void operator()(Object* object, MapPart* part, int object_index) const
 		{
 			if (!object->setSymbol(new_symbol, false))
 				part->deleteObject(object_index, false);
 			else
 				object->update();
-			return true;
 		}
 	};
 	
 	/** Delete objects. */
 	struct Delete
 	{
-		bool operator()(const Object*, MapPart* part, int object_index) const
+		void operator()(const Object*, MapPart* part, int object_index) const
 		{
 			part->deleteObject(object_index, false);
-			return true;
-		}
-	};
-	
-	/**
-	 * Can be used to check for the existence of certain types of objects
-	 * by checking if this operation would be applied to any object
-	 * under a given condition.
-	 */
-	struct NoOp
-	{
-		bool operator()(const Object*, const MapPart*, int) const noexcept
-		{
-			// Abort
-			return false;
 		}
 	};
 }

src/core/objects/object_query.cpp
@@ -349,17 +349,15 @@ void ObjectQuery::selectMatchingObjects(Map* map, MapEditorController* controlle
 	map->clearObjectSelection(false);
 
 	// Lambda to add objects to the selection
-	auto select_object = [map](Object* const object, MapPart*, int)
+	auto select_object = [map](Object* object)
 	{
 		map->addObjectToSelection(object, false);
-		return false; // applyOnMatchingObjects tells us if this op fails, so if we fail we made a selection
 	};
 
-	MapPart *part = map->getCurrentPart();
+	auto part = map->getCurrentPart();
+	part->applyOnMatchingObjects(select_object, *this);
 
-	// This reports failure if we made a selection
-	auto object_selected = !part->applyOnMatchingObjects(select_object, *this);
-
+	auto object_selected = !map->selectedObjects().empty();
 	if (object_selected || had_selection)
 		map->emitSelectionChanged();
 

src/core/objects/symbol_rule_set.cpp
@@ -294,7 +294,7 @@ void SymbolRuleSet::writeCrt(QTextStream& stream) const
 
 
 
-bool SymbolRuleSet::operator()(Object* object, MapPart*, int) const
+void SymbolRuleSet::operator()(Object* object) const
 {
 	for (const auto& item : *this)
 	{
@@ -304,7 +304,6 @@ bool SymbolRuleSet::operator()(Object* object, MapPart*, int) const
 			break;
 		}
 	}
-	return true;
 }
 
 

commit 730300057052b55724a16c9554d1d75c585ee28a

Fix Cut out tool
Broken since https://github.com/OpenOrienteering/mapper/commit/9bb9f2542b59cc66b1824765496af53f0183b2c6

src/tools/cutout_tool.cpp
@@ -177,6 +177,7 @@ struct PhysicalCutoutOperation
 	, cutout_object(cutout_object)
 	, cut_away(cut_away)
 	{
+		new_objects = new std::vector<PathObject*>();
 		add_step = new AddObjectsUndoStep(map);
 		delete_step = new DeleteObjectsUndoStep(map);
 	}
@@ -219,7 +220,7 @@ struct PhysicalCutoutOperation
 					return;
 				
 				add_step->addObject(object, object);
-				new_objects.insert(new_objects.end(), out_objects.begin(), out_objects.end());
+				new_objects->insert(new_objects->end(), out_objects.begin(), out_objects.end());
 			}
 			else
 			{
@@ -229,7 +230,7 @@ struct PhysicalCutoutOperation
 				boolean_tool.executeForLine(cutout_object, object->asPath(), out_objects);
 				
 				add_step->addObject(object, object);
-				new_objects.insert(new_objects.end(), out_objects.begin(), out_objects.end());
+				new_objects->insert(new_objects->end(), out_objects.begin(), out_objects.end());
 			}
 		}
 		
@@ -242,16 +243,16 @@ struct PhysicalCutoutOperation
 		
 		map->clearObjectSelection(false);
 		add_step->removeContainedObjects(false);
-		for (size_t i = 0; i < new_objects.size(); ++i)
+		for (size_t i = 0; i < new_objects->size(); ++i)
 		{
-			Object* object = new_objects[i];
+			Object* object = (*new_objects)[i];
 			map->addObject(object);
 		}
 		// Do not merge this loop into the upper one;
 		// theoretically undo step indices could be wrong this way.
-		for (size_t i = 0; i < new_objects.size(); ++i)
+		for (size_t i = 0; i < new_objects->size(); ++i)
 		{
-			Object* object = new_objects[i];
+			Object* object = (*new_objects)[i];
 			delete_step->addObject(part->findObjectIndex(object));
 		}
 		map->emitSelectionChanged();
@@ -289,7 +290,7 @@ private:
 	PathObject* cutout_object;
 	bool cut_away;
 	
-	std::vector<PathObject*> new_objects;
+	std::vector<PathObject*>* new_objects;
 	AddObjectsUndoStep* add_step;
 	DeleteObjectsUndoStep* delete_step;
 };