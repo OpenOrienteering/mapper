42623e007ab0de58200e4231a5f71f958f6e1838
Nikolay Korotkiy <sikmir@gmail.com>
2017-08-19 13:39:18 +0300

    translations: Add missing Qt messages

--- translations/qt_nl.ts
+++ translations/qt_nl.ts
@@ -678,9 +678,25 @@ Controleer of de juiste bestandsnaam is opgegeven.</translation>
 <context>
     <name>QPlatformTheme</name>
     <message>
+        <source>OK</source>
+        <translation></translation>
+    </message>
+    <message>
         <location filename="../src/gui/text_browser_dialog.cpp" line="63"/>
         <source>Close</source>
         <translation>Sluiten</translation>
+    </message>
+    <message>
+        <source>Cancel</source>
+        <translation></translation>
+    </message>
+    <message>
+        <source>Help</source>
+        <translation></translation>
+    </message>
+    <message>
+        <source>Reset</source>
+        <translation></translation>
     </message>
 </context>
 <context>
