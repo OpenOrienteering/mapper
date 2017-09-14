f6d66c9a9fd1b1915df4b2e5f4aaf0df8f59c019
Nikolay Korotkiy <sikmir@gmail.com>
2017-09-14 06:34:00 +0000

    Translated using Weblate (Finnish)
    Currently translated at 98.1% (1445 of 1472 strings)

--- translations/OpenOrienteering_fi.ts
+++ translations/OpenOrienteering_fi.ts
@@ -1536,7 +1536,7 @@
     <message>
         <location filename="../src/gui/widgets/general_settings_page.cpp" line="110"/>
         <source>Save undo/redo history</source>
-        <translation type="unfinished"></translation>
+        <translation>Tallenna kumoa/tee uudestaan historia</translation>
     </message>
     <message>
         <location filename="../src/gui/widgets/general_settings_page.cpp" line="113"/>
