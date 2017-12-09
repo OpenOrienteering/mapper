54b81f9d64234b379de732279c9c20d8e40875fa
naofum <naofum@gmail.com>
2017-12-09 06:45:59 +0000

    Translated using Weblate (Japanese)
    Currently translated at 100.0% (1464 of 1464 strings)

--- translations/OpenOrienteering_ja.ts
+++ translations/OpenOrienteering_ja.ts
@@ -4866,7 +4866,7 @@ selected</source>
     <message>
         <location filename="../src/gui/map/map_find_feature.cpp" line="103"/>
         <source>Query editor</source>
-        <translation type="unfinished"></translation>
+        <translation>クエリ エディター</translation>
     </message>
 </context>
 <context>
@@ -5464,7 +5464,7 @@ zoom using the mouse wheel, if available.</source>
         <location filename="../src/core/objects/object_query.cpp" line="305"/>
         <source>Text</source>
         <extracomment>Very short label</extracomment>
-        <translation type="unfinished"></translation>
+        <translation>テキスト</translation>
     </message>
     <message>
         <location filename="../src/core/objects/object_query.cpp" line="309"/>
