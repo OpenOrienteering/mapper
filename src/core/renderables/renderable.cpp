b48b4f26a4ca38e1e502a5bf92a14ca7298e8073
Kai Pastor <dg0yt@darc.de>
2018-12-23 21:44:05 +0100

    Revert "PainterConfig: Rewrite sub-pixel pen width handling"
    This reverts commit 0230da0e0f12607e08296775c80eb9a6eb4b03ea.

--- src/core/renderables/renderable.cpp
+++ src/core/renderables/renderable.cpp
@@ -1,6 +1,6 @@
 /*
  *    Copyright 2012, 2013 Thomas Schöps
- *    Copyright 2012-2018 Kai Pastor
+ *    Copyright 2012-2017 Kai Pastor
  *
  *    This file is part of OpenOrienteering.
  *
@@ -714,14 +714,12 @@ bool PainterConfig::activate(QPainter* painter, const QPainterPath*& current_cli
 	QBrush brush(config.testFlag(RenderConfig::Highlighted) ? highlightedColor(color) : color);
 	if (mode == PainterConfig::PenOnly)
 	{
-		if (pen_width > 0)
-		{
-			auto const width_px = pen_width * config.scaling;
-			if (config.testFlag(RenderConfig::Screen) && width_px < 0.125)
-				return false;
-			if (config.testFlag(RenderConfig::ForceMinSize) && width_px < 1)
-				actual_pen_width = 0.0; // Forces cosmetic pen
-		}
+#ifdef Q_OS_ANDROID
+		if (pen_width * config.scaling < 0.1)
+			return false;
+#endif
+		if (config.testFlag(RenderConfig::ForceMinSize) && pen_width * config.scaling <= 1.0)
+			actual_pen_width = 0.0; // Forces cosmetic pen
 		painter->setPen(QPen(brush, actual_pen_width));
 		painter->setBrush(QBrush(Qt::NoBrush));
 	}
