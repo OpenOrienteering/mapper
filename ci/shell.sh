d154282d5976256c14dc6ef22546a1df71b48b4a
Kai Pastor <dg0yt@darc.de>
2019-05-31 00:08:00 +0200

    CI: Use superbuild artifact for MSYS2

--- ci/shell.sh
+++ ci/shell.sh
@@ -1,3 +1,8 @@
 #!/bin/bash
+if [ -n "${MINGW}" ] ; then
+  source /etc/profile
+  unset CC
+  unset PKG_CONFIG_PATH
+fi
 set -x
-"$@" 3>&1 1>&2 2>&3 | sed -f "${BUILD_SOURCESDIRECTORY}/filter-stderr.sed" 3>&1 1>&2 2>&3
+"$@" 3>&1 1>&2 2>&3 | sed -f "${SOURCE_DIR}/filter-stderr.sed" 3>&1 1>&2 2>&3
