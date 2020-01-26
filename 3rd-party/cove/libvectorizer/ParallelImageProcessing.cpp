62abcd856b577eedf70f49ecbb5e4f9fa9468b82
Kai Pastor <dg0yt@darc.de>
2020-01-26 12:21:25 +0100

    cove: Test and fix HorizontalStripes::makeStripe()
    Handling color tables turned out to be not working as expected
    on the stripes which were meant to share the same const data.
    Based on insight into the implementation of QImage construction
    and assignment, it is possible to achieve the desired behaviour
    with another heap-allocated QImage sharing the same data.

--- 3rd-party/cove/libvectorizer/ParallelImageProcessing.cpp
+++ 3rd-party/cove/libvectorizer/ParallelImageProcessing.cpp
@@ -60,14 +60,38 @@ InplaceImage::~InplaceImage() = default;
 
 QImage HorizontalStripes::makeStripe(const QImage& original, int scanline, int stripe_height)
 {
-	QImage result(original.constScanLine(scanline),		        
-	              original.width(),
-	              std::min(scanline + stripe_height, original.height()) - scanline,
-	              original.bytesPerLine(),
-	              original.format() );
-	if (original.colorCount() > 0)
-		result.setColorTable(original.colorTable());
-	return result;
+	auto without_color_table = [](const QImage& original, int scanline, int stripe_height) -> QImage {
+		return QImage(original.constScanLine(scanline),
+		              original.width(),
+		              std::min(scanline + stripe_height, original.height()) - scanline,
+		              original.bytesPerLine(),
+		              original.format());
+	};
+	auto with_color_table = [](const QImage& original, int scanline, int stripe_height) -> QImage {
+		// Cf. https://bugreports.qt.io/browse/QTBUG-81674
+		// Setting the color table would cause detaching if we use the
+		// const char* taking constructor, so we const_cast<char*> to select
+		// the other constructor.
+		// But now we need a way to enforce detaching on non-const access to the
+		// returned image. We achieve this by creating a shared copy of the
+		// returned image carried in the cleanup data, and thus bound to its
+		// lifetime.
+		auto* cleanup_data = new QImage();
+		auto cleanup_function = [](void* data) { delete static_cast<QImage*>(data); };
+		QImage stripe(const_cast<uchar*>(original.constScanLine(scanline)),
+		              original.width(),
+		              std::min(scanline + stripe_height, original.height()) - scanline,
+		              original.bytesPerLine(),
+		              original.format(),
+		              cleanup_function,
+		              cleanup_data);
+		stripe.setColorTable(original.colorTable());
+		*cleanup_data = stripe;  // increase reference count -> force detach on write.
+		return stripe;
+	};
+	
+	auto const delegate = original.colorCount() == 0 ? without_color_table : with_color_table;
+	return delegate(original, scanline, stripe_height);
 }
 
 InplaceImage HorizontalStripes::makeStripe(QImage& original, int scanline, int stripe_height)
--- 3rd-party/cove/tests/ParallelImageProcessingTest.cpp
+++ 3rd-party/cove/tests/ParallelImageProcessingTest.cpp
@@ -17,12 +17,20 @@
  * along with this program. If not, see <http://www.gnu.org/licenses/>.
  */
 
+#include <algorithm>
+#include <iterator>
+#include <type_traits>
+
 #include <Qt>
+#include <QtGlobal>
 #include <QtTest>
 #include <QColor>
 #include <QImage>
 #include <QObject>
+#include <QRgb>
+#include <QSize>
 #include <QString>
+#include <QVector>
 
 #include "libvectorizer/ParallelImageProcessing.h"
 // IWYU pragma: no_include "libvectorizer/ProgressObserver.h"
@@ -87,6 +95,71 @@ private slots:
 			QCOMPARE(original.pixel(32,32), QColor(Qt::white).rgb());  // changed!
 		}
 	}
+	
+	
+	void makeStripeTest_data()
+	{
+		QTest::addColumn<QSize>("size");
+		QTest::addColumn<int>("scanline");
+		QTest::addColumn<int>("stripe_height");
+		QTest::addColumn<int>("expected_height");
+		QTest::newRow("stripe at begin") << QSize(100, 100) <<  0 << 50 << 50;
+		QTest::newRow("stripe at end")   << QSize(100, 100) << 75 << 25 << 25;
+		QTest::newRow("stripe over end") << QSize(100, 100) << 75 << 35 << 25;
+	}
+	
+	void makeStripeTest()
+	{
+		QFETCH(QSize, size);
+		QFETCH(int, scanline);
+		QFETCH(int, stripe_height);
+		QFETCH(int, expected_height);
+		
+		auto original = QImage(size, QImage::Format_Indexed8);
+		original.setColorTable({ QColor(Qt::red).rgb(), QColor(Qt::black).rgb() });
+		original.fill(0);
+		
+		auto const expected_color_table = original.colorTable();
+		QCOMPARE(expected_color_table.size(), 2);
+		
+		using std::begin;
+		using std::end;
+		
+		{
+			auto stripe_from_const = HorizontalStripes::makeStripe(static_cast<const QImage&>(original), scanline, stripe_height);
+			Q_STATIC_ASSERT((std::is_same<decltype(stripe_from_const), QImage>::value));
+			QCOMPARE(stripe_from_const.format(), original.format());
+			QCOMPARE(stripe_from_const.constBits(), original.constScanLine(scanline));
+			QCOMPARE(stripe_from_const.bytesPerLine(), original.bytesPerLine());
+			QCOMPARE(stripe_from_const.height(), expected_height);
+			auto const color_table = stripe_from_const.colorTable();
+			QVERIFY(std::equal(begin(color_table), end(color_table), begin(expected_color_table), end(expected_color_table)));
+			
+			QCOMPARE(original.pixelIndex(0, scanline), 0);
+			QCOMPARE(stripe_from_const.pixelIndex(0, 0), 0);
+			stripe_from_const.fill(1);
+			QCOMPARE(stripe_from_const.pixelIndex(0, 0), 1);     // new fill color
+			QCOMPARE(original.pixelIndex(0, scanline), 0);       // unchanged
+		}
+		
+		{
+			auto stripe_from_nonconst = HorizontalStripes::makeStripe(original, scanline, stripe_height);
+			Q_STATIC_ASSERT((std::is_same<decltype(stripe_from_nonconst), InplaceImage>::value));
+			QCOMPARE(stripe_from_nonconst.format(), original.format());
+			QCOMPARE(stripe_from_nonconst.constBits(), original.constScanLine(scanline));
+			QCOMPARE(stripe_from_nonconst.bytesPerLine(), original.bytesPerLine());
+			QCOMPARE(stripe_from_nonconst.height(), expected_height);
+			auto const color_table = stripe_from_nonconst.colorTable();
+			QVERIFY(std::equal(begin(color_table), end(color_table), begin(expected_color_table), end(expected_color_table)));
+			
+			QCOMPARE(original.pixelIndex(0, scanline), 0);
+			QCOMPARE(stripe_from_nonconst.pixelIndex(0, 0), 0);
+			stripe_from_nonconst.fill(1);
+			QCOMPARE(stripe_from_nonconst.pixelIndex(0, 0), 1);  // new fill color
+			QCOMPARE(original.pixelIndex(0, scanline), 1);       // new fill color
+		}
+	}
+	
 };
 	
 QTEST_GUILESS_MAIN(ParallelImageProcessingTest)
