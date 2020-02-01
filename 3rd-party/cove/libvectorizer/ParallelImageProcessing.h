8f776e819a7d52074f828fa9fd70d740ac979839
Kai Pastor <dg0yt@darc.de>
2020-02-01 07:19:40 +0100

    cove: Move stripe creation to worker threads
    We must carefully avoid having another temporary QImage implicitly
    sharing the stripe when a worker thread starts modifying the data,
    or the modifications would end in a detached location, leaving the
    stripe of the actual target unmodified.
    Fixes GH-1503 (Corrupted B/W image).

--- 3rd-party/cove/libvectorizer/ParallelImageProcessing.h
+++ 3rd-party/cove/libvectorizer/ParallelImageProcessing.h
@@ -18,8 +18,8 @@
  * along with this program. If not, see <http://www.gnu.org/licenses/>.
  */
 
-#ifndef PARALLELIMAGEPROCESSING_H
-#define PARALLELIMAGEPROCESSING_H
+#ifndef COVE_PARALLELIMAGEPROCESSING_H
+#define COVE_PARALLELIMAGEPROCESSING_H
 
 #include <algorithm>
 #include <iosfwd>
@@ -31,6 +31,8 @@
 
 namespace cove {
 
+class ProgressObserver;
+
 /**
  * Use concurrent image processing of horizontal stripes.
  */
@@ -41,6 +43,17 @@ struct HorizontalStripes
 	
 	/// Creates am InplaceImage stripe, meant for modification of the original image.
 	static QImage makeStripe(QImage& original, int scanline, int stripe_height);
+	
+	/// A structure to describe data to a job which modifies a target image stripe in-place.
+	struct InplaceStripeData
+	{
+		const QImage& source;
+		int source_scanline;
+		int source_height;
+		QImage& target;
+		int target_scanline;
+		int target_height;
+	};
 	
 	/// Creates concurrent jobs.
 	template <typename ResultType, typename Functor>
@@ -56,11 +69,21 @@ struct HorizontalStripes
 		auto const output_stripe_height = (target.height() + num_jobs - 1) / num_jobs;
 		for (int i = 0, j = 0; i < image_height; i += source_stripe_height, j += output_stripe_height)
 		{
-			jobs.emplace_back(Concurrency::run<ResultType>(
-			                      functor,
-			                      HorizontalStripes::makeStripe(source, i, source_stripe_height),
-			                      HorizontalStripes::makeStripe(target, j, output_stripe_height)
-			));
+			// If we construct the stripes here in the main thread, just before
+			// concurrent processing, there might be more than a single QImage
+			// referencing the same stripe when the worker thread modifies the
+			// stripe for the first time. In this situation, the modified QImage
+			// would detach, and the output would not reach the original target.
+			// That's why we build the stripes inside the concurrent job where
+			// it is going to be used, by using the following wrapper.
+			auto runner = [](const Functor& functor, InplaceStripeData& data, ProgressObserver& observer) -> ResultType {
+				auto const input = HorizontalStripes::makeStripe(data.source, data.source_scanline, data.source_height);
+				auto output = HorizontalStripes::makeStripe(data.target, data.target_scanline, data.target_height);
+				return functor(input, output, observer);
+			};
+			// Only a limited number of parameters can be passed to QtConcurrent::run.
+			auto data = InplaceStripeData { source, i, source_stripe_height, target, j, output_stripe_height };
+			jobs.emplace_back(Concurrency::run<ResultType>(runner, functor, data));
 		}
 		
 		return jobs;
@@ -70,4 +93,4 @@ struct HorizontalStripes
 
 }  // namespace cove
 
-#endif // PARALLELIMAGEPROCESSING_H
+#endif  // COVE_PARALLELIMAGEPROCESSING_H
