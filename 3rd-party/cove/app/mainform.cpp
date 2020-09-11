528c074090512d8df235a9d6bd22f26f73d80016
Libor Pechacek <lpechacek@gmx.com>
2020-09-11 14:31:09 +0200

    CoVe: Drop unused About dialog
    Part of GH-1295 task.

--- 3rd-party/cove/app/mainform.cpp
+++ 3rd-party/cove/app/mainform.cpp
@@ -1,5 +1,5 @@
 /*
- * Copyright (c) 2005-2019 Libor Pecháček.
+ * Copyright (c) 2005-2020 Libor Pecháček.
  * Copyright 2020 Kai Pastor
  *
  * This file is part of CoVe 
@@ -247,15 +247,6 @@ void mainForm::afterLoadImage()
 		}
 		setTabEnabled(ui.colorsTab, true);
 	}
-}
-
-//! Simple About dialog.
-void mainForm::aboutDialog()
-{
-	QMessageBox::about(this, tr("About"),
-					   trUtf8("<h3>COntour VEctorizer</h3><br />"
-							  "Author: Libor Pecháček<br />"
-							  "License: GPL"));
 }
 
 /*! Return color randomly created from image pixels.  It takes 5 scanlines and
