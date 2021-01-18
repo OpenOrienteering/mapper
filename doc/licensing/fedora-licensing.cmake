4ac96f0678cfea86a4dd987bc6dae362f1c1820c
Kai Pastor <dg0yt@darc.de>
2021-01-18 18:54:37 +0100

    Update fedora-licensing.cmake

--- doc/licensing/fedora-licensing.cmake
+++ doc/licensing/fedora-licensing.cmake
@@ -23,9 +23,9 @@ set(system_copyright_dir "/usr/share")
 set(copyright_pattern
   "${system_copyright_dir}/doc/@package@/COPYING"
   "${system_copyright_dir}/doc/@package@/COPYRIGHT"
-  "${system_copyright_dir}/doc/@package@-libs/LICENSE.txt" # gdal
+  "${system_copyright_dir}/doc/@package@-libs/LICENSE.TXT" # gdal
   "${system_copyright_dir}/licenses/@package@/COPYING"
   "${system_copyright_dir}/licenses/@package@/Copyright.txt"
   "${system_copyright_dir}/licenses/@package@/LICENSE"
-  "${system_copyright_dir}/licenses/@package@-libs/LICENSE.txt" # gdal
+  "${system_copyright_dir}/licenses/@package@-libs/LICENSE.TXT" # gdal
 )
