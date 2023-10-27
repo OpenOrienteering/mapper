767ea6f013b5e38d7b4ce41ae65efb6377a7ccb8
Libor Pecháček <lpechacek@suse.cz>
2023-10-27 19:07:55 +0000

    Translated using Weblate (Basque)
    Currently translated at 0.3% (5 of 1480 strings)

--- translations/OpenOrienteering_eu.ts
+++ translations/OpenOrienteering_eu.ts
@@ -1398,7 +1398,7 @@
     <message>
         <location filename="../src/fileformats/ocd_file_export.cpp" line="649"/>
         <source>Could not write file: %1</source>
-        <translation>Ezin izan da fitxategia idatzi:% 1</translation>
+        <translation>Ezin izan da fitxategia idatzi:%1</translation>
     </message>
     <message>
         <location filename="../src/fileformats/file_import_export.cpp" line="338"/>
