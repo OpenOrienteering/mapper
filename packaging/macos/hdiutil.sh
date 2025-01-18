ca89d330c3e4b8b4de3db66d7c4ad81b4ba31cba
Kai Pastor <dg0yt@darc.de>
2025-01-18 08:39:46 +0100

    Packaging: Retry 'hdiutil create'
    Mitigate interference from XProtect.

---
+++ packaging/macos/hdiutil.sh
@@ -0,0 +1,24 @@
+#!/bin/sh
+
+# Mitigate XProtect race condition for "hdiutil create"
+# Cf. https://github.com/actions/runner-images/issues/7522
+
+set -e
+set -x
+
+case "$1" in
+create)
+    max=5
+    ;;
+*)
+    max=1
+    ;;
+esac
+
+i=1
+until hdiutil "$@"
+do
+    if [ $i -eq $max ]; then exit 1; fi
+    sleep 1
+    i=$((i+1))
+done
