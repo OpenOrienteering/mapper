f99a5d907583ac02724136352e7662c2c9f275ca
Kai Pastor <dg0yt@darc.de>
2025-02-16 21:16:00 +0100

    Deprecate LICENSE_PROVIDER=ubuntu
    We can rely on ID_LIKE debian now. However, developers may have
    ubuntu in CMake cache, so we can remove ubuntu-licensing.cmake
    only after some transitional period.

--- doc/licensing/ubuntu-licensing.cmake
+++ doc/licensing/ubuntu-licensing.cmake
@@ -1,1 +1,3 @@
+message(WARNING "LICENSING_PROVIDER=ubuntu is deprecated. Switching to debian.")
+set(LICENSING_PROVIDER "debian" CACHE STRING "Provider for 3rd-party licensing information" FORCE)
 include("${CMAKE_CURRENT_LIST_DIR}/debian-licensing.cmake")
