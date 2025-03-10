e527095deddd931846b821ead29a22ef5271d5b8
Matthias Kuehlewein <github@kuehlewein.de>
2025-03-10 17:13:43 +0100

    GeoreferencingDialog: cleanup

--- src/gui/georeferencing_dialog.cpp
+++ src/gui/georeferencing_dialog.cpp
@@ -1,6 +1,6 @@
 /*
  *    Copyright 2012, 2013 Thomas Schöps
- *    Copyright 2012-2020, 2024 Kai Pastor
+ *    Copyright 2012-2020, 2024, 2025 Kai Pastor
  *
  *    This file is part of OpenOrienteering.
  *
@@ -23,7 +23,6 @@
 
 #include <cmath>
 #include <functional>
-#include <vector>
 
 #include <Qt>
 #include <QtGlobal>
@@ -237,7 +236,6 @@ GeoreferencingDialog::GeoreferencingDialog(
 	  Qt::Horizontal);
 	reset_button = buttons_box->button(QDialogButtonBox::Reset);
 	reset_button->setEnabled(initial);
-	auto help_button = buttons_box->button(QDialogButtonBox::Help);
 	
 	auto edit_layout = new QFormLayout();
 	
@@ -251,7 +249,6 @@ GeoreferencingDialog::GeoreferencingDialog(
 	edit_layout->addRow(tr("Map coordinates:"), map_ref_layout);
 	edit_layout->addRow(projected_ref_label, projected_ref_layout);
 	edit_layout->addRow(tr("Geographic coordinates:"), geographic_ref_layout);
-	edit_layout->addRow(show_refpoint_label, link_label);
 	edit_layout->addRow(show_refpoint_label, link_label);
 	edit_layout->addRow(tr("On CRS changes, keep:"), keep_projected_radio);
 	edit_layout->addRow({}, keep_geographic_radio);
@@ -301,7 +298,7 @@ GeoreferencingDialog::GeoreferencingDialog(
 	connect(buttons_box, &QDialogButtonBox::accepted, this, &GeoreferencingDialog::accept);
 	connect(buttons_box, &QDialogButtonBox::rejected, this, &GeoreferencingDialog::reject);
 	connect(reset_button, &QPushButton::clicked, this, &GeoreferencingDialog::reset);
-	connect(help_button, &QPushButton::clicked, this, &GeoreferencingDialog::showHelp);
+	connect(buttons_box, &QDialogButtonBox::helpRequested, this, &GeoreferencingDialog::showHelp);
 	
 	connect(georef.data(), &Georeferencing::stateChanged, this, &GeoreferencingDialog::georefStateChanged);
 	connect(georef.data(), &Georeferencing::transformationChanged, this, &GeoreferencingDialog::transformationChanged);
