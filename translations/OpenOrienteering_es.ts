41dc8460154266c63ca96b9863a3bf37aaf8e283
Matthias Kühlewein <github@kuehlewein.de>
2025-03-21 09:43:38 +0100

    Translated using Weblate (Spanish)
    Currently translated at 96.4% (1480 of 1535 strings)

--- translations/OpenOrienteering_es.ts
+++ translations/OpenOrienteering_es.ts
@@ -5149,7 +5149,7 @@ selecc</translation>
     <message>
         <location filename="../src/gui/map/map_editor.cpp" line="2633"/>
         <source>Resulting symbol: %1.</source>
-        <translation type="unfinished">Símbolo resultante: %1 %2. {1.?}</translation>
+        <translation>Símbolo resultante: %1.</translation>
     </message>
     <message>
         <location filename="../src/gui/map/map_editor.cpp" line="2667"/>
