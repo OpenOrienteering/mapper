7503e954d103446607fe4718113b6ed2d3f1ca19
Kai Pastor <dg0yt@darc.de>
2025-05-05 08:57:51 +0200

    OcdFileImport: All fill patterns are rotatable
    This change reverts the effect of 918dd9b.

--- src/fileformats/ocd_file_import.cpp
+++ src/fileformats/ocd_file_import.cpp
@@ -1,5 +1,5 @@
 /*
- *    Copyright 2013-2022, 2024 Kai Pastor
+ *    Copyright 2013-2022, 2024, 2025 Kai Pastor
  *
  *    Some parts taken from file_format_oc*d8{.h,_p.h,cpp} which are
  *    Copyright 2012 Pete Curtis
@@ -1570,6 +1570,8 @@ Symbol* OcdFileImport::importAreaSymbol(const S& ocd_symbol)
 
 void OcdFileImport::setupAreaSymbolCommon(OcdImportedAreaSymbol* symbol, bool rotatable, bool fill_on, const Ocd::AreaSymbolCommonV8& ocd_symbol, std::size_t data_size, const Ocd::PointSymbolElementV8* elements)
 {
+	Q_UNUSED(rotatable)  // To be done: Control rotate-map behavior
+	
 	// Basic area symbol fields: minimum_area, color
 	symbol->minimum_area = 0;
 	symbol->color = fill_on ? convertColor(ocd_symbol.fill_color) : nullptr;
@@ -1582,7 +1584,7 @@ void OcdFileImport::setupAreaSymbolCommon(OcdImportedAreaSymbol* symbol, bool ro
 		AreaSymbol::FillPattern pattern;
 		pattern.type = AreaSymbol::FillPattern::LinePattern;
 		pattern.angle = convertAngle(ocd_symbol.hatch_angle_1);
-		pattern.setRotatable(rotatable);
+		pattern.setRotatable(true);
 		pattern.line_spacing = convertLength(ocd_symbol.hatch_dist);
 		pattern.line_offset = 0;
 		pattern.line_width = convertLength(ocd_symbol.hatch_line_width);
@@ -1607,7 +1609,7 @@ void OcdFileImport::setupAreaSymbolCommon(OcdImportedAreaSymbol* symbol, bool ro
 		AreaSymbol::FillPattern pattern;
 		pattern.type = AreaSymbol::FillPattern::PointPattern;
 		pattern.angle = convertAngle(ocd_symbol.structure_angle);
-		pattern.setRotatable(rotatable);
+		pattern.setRotatable(true);
 		pattern.point_distance = convertLength(ocd_symbol.structure_width);
 		pattern.line_spacing = convertLength(ocd_symbol.structure_height);
 		pattern.line_offset = 0;
