a9e49a55c5c2b314abb8f00f01a1c4452f797ae0
Matthias Kuehlewein <github@kuehlewein.de>
2025-10-03 16:54:08 +0200

    MapPart: Maintain visibility when merging all parts
    When merging all parts, maintain visibility of the part into which the
    other are merged.

--- src/gui/map/map_editor.cpp
+++ src/gui/map/map_editor.cpp
@@ -3982,6 +3982,7 @@ void MapEditorController::mergeCurrentMapPartTo(int target)
 void MapEditorController::mergeAllMapParts()
 {
 	QString const name = map->getCurrentPart()->getName();
+	const auto visibility = map->getCurrentPart()->isVisible();
 	const QMessageBox::StandardButton button =
 	        QMessageBox::question(
 	            window,
@@ -3994,7 +3995,7 @@ void MapEditorController::mergeAllMapParts()
 		auto* undo = new CombinedUndoStep(map);
 		
 		// For simplicity, we merge to the first part,
-		// but keep the properties (i.e. name) of the current part.
+		// but keep the properties (i.e. name, visibility) of the current part.
 		map->setCurrentPartIndex(0);
 		MapPart* target_part = map->getPart(0);
 		
@@ -4011,6 +4012,7 @@ void MapEditorController::mergeAllMapParts()
 		
 		undo->push(new MapPartUndoStep(map, MapPartUndoStep::ModifyMapPart, 0));
 		target_part->setName(name);
+		target_part->setVisible(visibility);
 		
 		map->push(undo);
 	}
