99b367aac78ef8b1ae6898c194dea1dd8dd0fe51
Kai Pastor <dg0yt@darc.de>
2026-02-27 00:26:14 +0100

    TemplateTest: Fix Unicode string initialization

--- test/template_t.cpp
+++ test/template_t.cpp
@@ -361,7 +361,7 @@ private slots:
 		
 		QCOMPARE(map.getNumTemplates(), 1);
 		auto temp = map.getTemplate(0);
-		QCOMPARE(temp->getTemplateFilename(), QString::fromUtf8("\u0433\u0435\u043E.tiff"));
+		QCOMPARE(temp->getTemplateFilename(), QString::fromWCharArray(L"\u0433\u0435\u043E.tiff"));
 #ifndef MAPPER_USE_GDAL
 		QCOMPARE(temp->getTemplateType(), "TemplateImage");
 #else
